1c31bac2737e533413c3133c590ab7788bacc112
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2014-02-06 15:40:05 +0100

    Print router errors and respond with error code

--- lib/explex_web/router.ex
+++ lib/explex_web/router.ex
@@ -2,6 +2,35 @@ defmodule ExplexWeb.Router do
   use Plug.Router
   import Plug.Connection
 
+  def call(conn, _opts) do
+    try do
+      dispatch(conn.method, conn.path_info, conn)
+    catch
+      kind, error ->
+        print_error(kind, error, System.stacktrace)
+        if impl = Plug.Exception.impl_for(error) do
+          { :halt, send_resp(conn, impl.status(error), "") }
+        else
+          { :halt, send_resp(conn, 500, "") }
+        end
+    end
+  end
+
+  defp print_error(:error, exception, stacktrace) do
+    IO.inspect ""
+    exception = Exception.normalize(exception)
+    IO.puts IO.ANSI.escape_fragment("\n%{red}** (#{inspect exception.__record__(:name)}) #{exception.message}", true)
+    IO.puts IEx.Evaluator.format_stacktrace(stacktrace)
+  end
+
+  defp print_error(kind, reason, stacktrace) do
+    IO.puts IO.ANSI.escape_fragment("\n%{red}** (#{kind}) #{inspect(reason)}", true)
+    IO.puts IEx.Evaluator.format_stacktrace(stacktrace)
+  end
+
+  match _ do
+    { :halt, send_resp(conn, 404, "") }
+  end
   match _ do
     { :ok, send_resp(conn, 404, "") }
   end
