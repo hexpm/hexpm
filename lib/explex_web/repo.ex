d02b478a3cbdca043b122203b7d95b08bc6d1511
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2014-02-16 19:49:20 +0100

    Add support to test client with server

--- lib/explex_web/repo.ex
+++ lib/explex_web/repo.ex
@@ -1,8 +1,11 @@
 defmodule ExplexWeb.Repo do
   use Ecto.Repo, adapter: Ecto.Adapters.Postgres, env: Mix.env
 
-  def url(:dev),
-    do: "ecto://explex:explex@localhost/explex_dev"
+  def url(:dev) do
+    # Special case for integration testing explex client
+    System.get_env("EXPLEX_ALT_ECTO_URL") ||
+    "ecto://explex:explex@localhost/explex_dev"
+  end
 
   def url(:test),
     do: "ecto://explex:explex@localhost/explex_test?size=1&max_overflow=0"
