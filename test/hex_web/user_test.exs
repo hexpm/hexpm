9f18c38211115e7c7e9d617e93ce5251be9b5bb7
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2014-03-03 18:23:56 +0100

    Add update user test

--- test/hex_web/user_test.exs
+++ test/hex_web/user_test.exs
@@ -19,4 +19,14 @@ defmodule HexWeb.UserTest do
     assert { :error, _ } = User.create("eric", "mail@mail.com", "pass")
     assert { :error, _ } = User.create("name", "eric@mail.com", "pass")
   end
+
+  test "update user" do
+    assert { :ok, user } = User.create("eric", "eric@mail.com", "erics_pass")
+    User.update(user, "other@mail.com", "new_pass")
+
+    user = User.get("eric")
+    assert user.email == "other@mail.com"
+    assert User.auth?(user, "new_pass")
+    refute User.auth?(user, "erics_pass")
+  end
 end
