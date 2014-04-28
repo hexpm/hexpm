83dbf146127527c688da53586d6977f328d3992c
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2014-04-28 23:40:54 +0200

    Support version 2 tarballs with sha-2 checksums

--- lib/hex_web/tar.ex
+++ lib/hex_web/tar.ex
@@ -10,7 +10,8 @@ defmodule HexWeb.Tar do
   defmacrop if_ok(expr, call) do
     quote do
       case unquote(expr) do
-        { :ok, var } -> unquote(Macro.pipe(quote(do: var), call, 0))
+        { :ok, var1, var2 } ->
+          unquote(Macro.pipe(quote(do: var1), Macro.pipe(quote(do: var2), call, 0), 0))
         other -> other
       end
     end
@@ -21,10 +22,10 @@ defmodule HexWeb.Tar do
       { :ok, files } ->
         files = Enum.into(files, %{}, fn { name, binary } -> { String.from_char_data!(name), binary } end)
 
-        missing_files(files)
-        |> if_ok(unknown_files)
+        version(files)
         |> if_ok(checksum)
-        |> if_ok(version)
+        |> if_ok(missing_files)
+        |> if_ok(unknown_files)
         |> if_ok(meta)
 
       { :error, reason } ->
@@ -32,48 +33,57 @@ defmodule HexWeb.Tar do
     end
   end
 
-  defp missing_files(files) do
+  defp version(files) do
+    version = files["VERSION"]
+    if version in ["1", "2"] do
+      { :ok, files, binary_to_integer(version) }
+    else
+      { :error, %{version: :wrong} }
+    end
+  end
+
+  defp checksum(files, version) do
+    blob = files["VERSION"] <> files["metadata.exs"] <> files["contents.tar.gz"]
+    if hash(blob, version) == HexWeb.Util.dehexify(files["CHECKSUM"]) do
+      { :ok, files, version }
+    else
+      { :error, %{checksum: :wrong} }
+    end
+  end
+
+  defp missing_files(files, version) do
     missing_files = Enum.reject(@files, &Dict.has_key?(files, &1))
     if length(missing_files) == 0 do
-      { :ok, files }
+      { :ok, files, version }
     else
       { :error, %{missing_files: missing_files} }
     end
   end
 
-  defp unknown_files(files) do
+  defp unknown_files(files, version) do
     unknown_files = Enum.reject(files, fn { name, _binary } -> name in @files end)
     if length(unknown_files) == 0 do
-      { :ok, files }
+      { :ok, files, version }
     else
       names = Enum.map(unknown_files, &elem(&1, 0))
       { :error, %{unknown_files: names} }
     end
   end
 
-  defp checksum(files) do
-    blob = files["VERSION"] <> files["metadata.exs"] <> files["contents.tar.gz"]
-    if :crypto.hash(:md5, blob) == HexWeb.Util.dehexify(files["CHECKSUM"]) do
-      { :ok, files }
-    else
-      { :error, %{checksum: :wrong} }
-    end
-  end
-
-  defp version(files) do
-    if files["VERSION"] == "1" do
-      { :ok, files }
-    else
-      { :error, %{version: :wrong} }
-    end
-  end
-
-  defp meta(files) do
+  defp meta(files, _version) do
     try do
       { :ok, HexWeb.Util.safe_deserialize_elixir(files["metadata.exs"]) }
     rescue
       err in [HexWeb.Util.BadRequest] ->
         { :error, %{metadata: err.message} }
     end
+  end
+
+  defp hash(blob, 1) do
+    :crypto.hash(:md5, blob)
+  end
+
+  defp hash(blob, 2) do
+    :crypto.hash(:sha256, blob)
   end
 end
