25cc1b473a42da7e52896e8e22141d99896ebdb3
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2014-07-05 02:22:17 +0200

    Revert "Remove list_to_map conversion"
    This reverts commit 29a514e75f2bcae517cb0442594dc69793e5f2b3.

--- lib/hex_web/api/elixir_format.ex
+++ lib/hex_web/api/elixir_format.ex
@@ -29,6 +29,7 @@ defmodule HexWeb.API.ElixirFormat do
     if safe_term?(ast) do
       result = Code.eval_quoted(ast)
                |> elem(0)
+               |> list_to_map
       {:ok, result}
     else
       {:error, "unsafe elixir"}
@@ -61,4 +62,19 @@ defmodule HexWeb.API.ElixirFormat do
   defp safe_term?(term) when is_list(term), do: Enum.all?(term, &safe_term?/1)
   defp safe_term?(term) when is_tuple(term), do: Enum.all?(Tuple.to_list(term), &safe_term?/1)
   defp safe_term?(_), do: false
+
+  defp list_to_map(list) when is_list(list) do
+    if list == [] or is_tuple(List.first(list)) do
+      Enum.into(list, %{}, fn
+        {key, list} when is_list(list) -> {key, list_to_map(list)}
+        other -> list_to_map(other)
+      end)
+    else
+      Enum.map(list, &list_to_map/1)
+    end
+  end
+
+  defp list_to_map(other) do
+    other
+  end
 end
