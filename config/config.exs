dd89e4997e7da521b33dd3674e10289a9771a489
James Spencer <jamess237@icloud.com>
2014-08-10 20:09:22 +1000

    Change from List > Binary

--- config/config.exs
+++ config/config.exs
@@ -18,7 +18,7 @@ config :hex_web,
   url:      System.get_env("HEX_URL") || "http://localhost:4000",
   app_host: System.get_env("APP_HOST"),
 
-  s3_url:        System.get_env("S3_URL") || 'http://s3.amazonaws.com',
+  s3_url:        System.get_env("S3_URL") || "http://s3.amazonaws.com",
   s3_bucket:     System.get_env("S3_BUCKET"),
   s3_access_key: System.get_env("S3_ACCESS_KEY"),
   s3_secret_key: System.get_env("S3_SECRET_KEY"),
