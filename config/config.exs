8f1091e6536dece5b5bbf6ee1885f9d750c19110
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2014-08-20 21:40:27 +0200

    Don't use default secret in production

--- config/config.exs
+++ config/config.exs
@@ -15,16 +15,24 @@ end
 config :hex_web,
   password_work_factor: work_factor,
 
-  url:      System.get_env("HEX_URL") || "http://localhost:4000",
+  url:      System.get_env("HEX_URL"),
   app_host: System.get_env("APP_HOST"),
 
   s3_url:        System.get_env("S3_URL") || "http://s3.amazonaws.com",
   s3_bucket:     System.get_env("S3_BUCKET"),
   s3_access_key: System.get_env("S3_ACCESS_KEY"),
   s3_secret_key: System.get_env("S3_SECRET_KEY"),
-  cdn_url:       System.get_env("CDN_URL") || "http://localhost:4000",
+  cdn_url:       System.get_env("CDN_URL"),
 
-  secret: System.get_env("HEX_SECRET") || "796f75666f756e64746865686578"
+  secret: System.get_env("HEX_SECRET")
+
+if Mix.env in [:dev, :test] do
+  config :hex_web,
+    url:     System.get_env("HEX_URL")    || "http://localhost:4000",
+    cdn_url: System.get_env("CDN_URL")    || "http://localhost:4000",
+    secret:  System.get_env("HEX_SECRET") || "796f75666f756e64746865686578"
+end
+
 
 config :logger,
   level: log_level
