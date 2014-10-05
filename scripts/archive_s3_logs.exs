9c8c25bf1e06d09310b07adf85b969f34d6106d4
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2014-10-05 21:50:04 +0200

    Update archive logs task to use new bucket

--- scripts/archive_s3_logs.exs
+++ scripts/archive_s3_logs.exs
@@ -1,14 +1,15 @@
 [date] = System.argv
-File.mkdir("logs")
-
-IO.puts :os.cmd('aws s3 cp s3://s3.hex.pm . --recursive --exclude "*" --include "logs/#{date}*"')
+File.mkdir_p!("logs/hex")
 File.cd!("logs")
 
+IO.puts :os.cmd('aws s3 cp s3://logs.hex.pm . --recursive --exclude "*" --include "hex/#{date}*"')
+
+File.cd!("hex")
 IO.puts :os.cmd('zip -9 -r #{date}.zip .')
 
 IO.puts :os.cmd('aws s3 cp #{date}.zip s3://s3.hex.pm/log-archives/#{date}.zip')
 
-IO.puts :os.cmd('aws s3 rm s3://s3.hex.pm --recursive --exclude "*" --include "logs/#{date}*"')
+IO.puts :os.cmd('aws s3 rm s3://logs.hex.pm --recursive --exclude "*" --include "hex/#{date}*"')
 
-File.cd!("..")
+File.cd!("../..")
 File.rm_rf!("logs")
