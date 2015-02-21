75148a98b419455496ea523f911bbfd3740be262
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2015-02-22 00:40:52 +0100

    Update log sync script

--- scripts/s3_log_sync.exs
+++ scripts/s3_log_sync.exs
@@ -1,2 +1,2 @@
-File.mkdir("hex")
-HexWeb.Util.shell(~s(aws s3 sync s3://logs.hex.pm . --delete --exclude "*" --include "hex/*"))
+File.mkdir("../logs/hex")
+HexWeb.Util.shell(~s(aws s3 cp s3://logs.hex.pm ../logs --recursive --exclude "*" --include "hex/*"))
