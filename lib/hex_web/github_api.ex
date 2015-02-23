ec3694074c21359399058d465e4dd2f5741b13c1
Blackrush <blackrushx@gmail.com>
2015-02-23 20:56:42 +0100

    Clean up HexWeb.GithubApi module

--- lib/hex_web/github_api.ex
+++ lib/hex_web/github_api.ex
@@ -1,37 +1,32 @@
 defmodule HexWeb.GithubApi do
   use HTTPoison.Base
 
-  defp build_path([hd]) do
-    to_string(hd)
-  end
-  defp build_path([hd|tl]) do
-    "#{hd}/#{build_path(tl)}"
+  defp find_package_name(title), do: String.replace(title, ~r/(Add Package )|"/, "")
+
+  defp build_path(list), do: Enum.join(list, "/")
+  defp build_params(list) do
+    list |> Enum.map(fn({k, v}) -> "#{k}=#{v}" end)
+         |> Enum.join("&")
   end
 
-  defp build_params([]) do
-    ""
-  end
-  defp build_params([{k, v}]) do
-    "#{k}=#{v}"
-  end
-  defp build_params([{k, v}|tl]) do
-    "#{k}=#{v}&#{build_params(tl)}"
-  end
+  @doc false
+  def process_url(url), do: "https://api.github.com" <> url
+  @doc false
+  def process_response_body(body), do: Poison.decode!(body)
 
+  @doc """
+  Send a GET request and return its JSON response.
+  """
+  @spec read([String], Keyword) :: term
   def read(path, params) do
     url = "/" <> build_path(path) <> "?" <> build_params(params)
     get!(url).body
   end
 
-  def process_url(url) do
-    "https://api.github.com" <> url
-  end
-
-  def process_response_body(body) do
-    body |> Poison.decode!
-         # |> Enum.map(fn {k,v} -> {:"#{k}", v} end)
-  end
-
+  @doc """
+  Get the last closed issues of a repository.
+  """
+  @spec read(String, Integer) :: [term]
   def last_closed_issues(repo, count) do
     read ["repos", repo, "issues"],
       state:    :closed,
@@ -39,15 +34,13 @@ defmodule HexWeb.GithubApi do
       per_page: count
   end
 
-  defp find_package_name(title) do
-    title
-      |> String.replace(~r/(Add Package )|"/, "")
-  end
-
+  @doc """
+  Get the last added packages to an awesome-elixir list.
+  """
   def last_awesome_packages(repo, count) do
     last_closed_issues(repo, count)
       |> Enum.filter_map(fn(x) -> !x["pull_request"] end,
                          fn(x) -> find_package_name(x["title"]) end)
   end
-
 end
+
