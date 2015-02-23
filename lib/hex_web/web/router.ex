f9ee5b6be5b6372bd7ce3fac4c289d2d80d00762
Blackrush <blackrushx@gmail.com>
2015-02-23 19:34:56 +0100

    Display awesomeness on the front page

--- lib/hex_web/web/router.ex
+++ lib/hex_web/web/router.ex
@@ -25,9 +25,10 @@ defmodule HexWeb.Web.Router do
     package_new  = Package.recent(10)
     package_top  = PackageDownload.top(:all, 10)
     total        = PackageDownload.total
+    awesomeness  = Package.awesomeness(10)
 
     conn = assign_pun(conn, [num_packages, num_releases, package_top,
-                             package_new, releases_new, total])
+                             package_new, releases_new, total, awesomeness])
     send_page(conn, :index)
   end
 
