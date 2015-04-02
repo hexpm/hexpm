945a6773e6210c4e55a639355e77739bcca7fc6d
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2015-04-02 14:08:49 -0500

    Use elixir v1.0.4-dev

--- mix.exs
+++ mix.exs
@@ -5,6 +5,8 @@ defmodule HexWeb.Mixfile do
     [app: :hex_web,
      version: "0.0.1",
      elixir: "~> 1.0",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
      aliases: aliases,
      deps: deps]
   end
