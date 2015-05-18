cffce80edbe36ec78cd1891f29a43ed2eafeeba6
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2015-05-18 12:08:53 +0200

    Update archiving script

--- scripts/archive_s3_logs.exs
+++ scripts/archive_s3_logs.exs
@@ -1,15 +1,15 @@
-[date] = System.argv
-File.mkdir_p!("logs/hex")
+[dir, date] = System.argv
+File.mkdir_p!("logs/#{dir}")
 File.cd!("logs")
 
-IO.puts :os.cmd('aws s3 cp s3://logs.hex.pm . --recursive --exclude "*" --include "hex/#{date}*"')
+IO.puts :os.cmd('aws s3 cp s3://logs.hex.pm . --recursive --exclude "*" --include "#{dir}/#{date}*"')
 
-File.cd!("hex")
+File.cd!(dir)
 IO.puts :os.cmd('zip -9 -r #{date}.zip .')
 
-IO.puts :os.cmd('aws s3 cp #{date}.zip s3://s3.hex.pm/log-archives/#{date}.zip')
+IO.puts :os.cmd('aws s3 cp #{date}.zip s3://s3.hex.pm/log-archives/#{dir}-#{date}.zip')
 
-IO.puts :os.cmd('aws s3 rm s3://logs.hex.pm --recursive --exclude "*" --include "hex/#{date}*"')
+IO.puts :os.cmd('aws s3 rm s3://logs.hex.pm --recursive --exclude "*" --include "#{dir}/#{date}*"')
 
 File.cd!("../..")
 File.rm_rf!("logs")
