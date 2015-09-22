842761051406350b56051a917b674fa3126d0e97
Tonći Galić <tuxified@gmail.com>
2015-09-22 14:39:07 +0200

    feat(package): add copy config snippet button
    Fixes hexpm/hex_web#110 by adding a copy button (copy glyphicon, hence the
    bootswatch upgrade).

--- priv/static/static/main.js
+++ priv/static/static/main.js
@@ -10,3 +10,26 @@ $('.show-versions .toggle-text').click(function() {
   $(this).find('span').toggle();
 });
 
+// Package: copy config snippet to clipboard
+function copy_snippet(element_id, button) {
+  try {
+    snippet = document.getElementById(element_id);
+    snippet.select();
+
+    if( document.execCommand('copy') ) { copy_succeeded(button); }
+    else { copy_failed(button); }
+  } catch (e) {
+    console.log('snippet copy failed', e);
+    copy_failed(button);
+  }
+}
+
+function copy_succeeded(button) {
+  $(button).children().removeClass("glyphicon-copy").addClass("glyphicon-ok green");
+  setTimeout(function() { $(button).children().removeClass("glyphicon-ok green").addClass("glyphicon-copy") }, 1500);
+}
+
+function copy_failed(button) {
+  $(button).children().removeClass("glyphicon-copy").addClass("glyphicon-remove");
+  setTimeout(function() { $(button).children().removeClass("glyphicon-remove").addClass("glyphicon-copy") }, 1500);
+}
