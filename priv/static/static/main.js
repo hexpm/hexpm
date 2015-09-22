2da51c331dc28f5a88da9e699bcf4babce6d2137
Tonći Galić <tuxified@gmail.com>
2015-09-22 16:58:37 +0200

    feat(snippet): make copy feedback prettier
    using tooltips to show if copy succeeded

--- priv/static/static/main.js
+++ priv/static/static/main.js
@@ -12,24 +12,28 @@ $('.show-versions .toggle-text').click(function() {
 
 // Package: copy config snippet to clipboard
 function copy_snippet(element_id, button) {
+  succeeded = false;
   try {
     snippet = document.getElementById(element_id);
     snippet.select();
 
-    if( document.execCommand('copy') ) { copy_succeeded(button); }
-    else { copy_failed(button); }
+    succeeded = document.execCommand('copy')
   } catch (e) {
     console.log('snippet copy failed', e);
-    copy_failed(button);
   }
+
+  if(succeeded) { copy_succeeded(button) }
+  else { copy_failed(button); }
 }
 
 function copy_succeeded(button) {
   $(button).children().removeClass("glyphicon-copy").addClass("glyphicon-ok green");
-  setTimeout(function() { $(button).children().removeClass("glyphicon-ok green").addClass("glyphicon-copy") }, 1500);
+  $(button).tooltip({ title: 'Copied!', container: 'body', placement: 'bottom', trigger: 'manual' }).tooltip('show');
+  setTimeout(function() { $(button).children().removeClass("glyphicon-ok green").addClass("glyphicon-copy"); $(button).tooltip("hide")  }, 1500);
 }
 
 function copy_failed(button) {
   $(button).children().removeClass("glyphicon-copy").addClass("glyphicon-remove");
-  setTimeout(function() { $(button).children().removeClass("glyphicon-remove").addClass("glyphicon-copy") }, 1500);
+  $(button).tooltip({ title: 'Copy failed :(', container: 'body', placement: 'bottom', trigger: 'manual' }).tooltip('show');
+  setTimeout(function() { $(button).children().removeClass("glyphicon-remove").addClass("glyphicon-copy"); $(button).tooltip("hide")  }, 1500);
 }
