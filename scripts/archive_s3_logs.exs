a7d780d20b5925cafd3a0347ac8a44f4bccbb041
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2015-10-11 11:08:17 -0500

    Bundle archived logs into singel file

--- scripts/archive_s3_logs.exs
+++ scripts/archive_s3_logs.exs
@@ -4,10 +4,15 @@ File.cd!("logs")
 
 HexWeb.Util.shell(~s(aws s3 cp s3://logs.hex.pm . --recursive --exclude "*" --include "#{dir}/#{date}*"))
 
-File.cd!(dir)
-HexWeb.Util.shell(~s(zip -9 -r #{date}.zip .))
+contents =
+  Path.wildcard("#{dir}/**")
+  |> Enum.reduce([], fn file, acc -> [acc|File.read!(file)] end)
+  |> :zlib.gzip
 
-HexWeb.Util.shell(~s(aws s3 cp #{date}.zip s3://s3.hex.pm/log-archives/#{dir}-#{date}.zip))
+filename = "#{dir}-#{date}.txt.gz"
+File.write!(filename, contents)
+
+HexWeb.Util.shell(~s(aws s3 cp #{filename} s3://s3.hex.pm/log-archives/#{filename}))
 
 HexWeb.Util.shell(~s(aws s3 rm s3://logs.hex.pm --recursive --exclude "*" --include "#{dir}/#{date}*"))
 
