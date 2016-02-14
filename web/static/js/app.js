214a97cb792bf09586c6dccb96e6167da3d78c92
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2016-02-14 02:41:19 +0100

    Fix collapse of package versions

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -25,9 +25,6 @@ export var App = {
     // Show show-versions button if JS is enabled
     $('.show-versions').show();
 
-    // Hide older versions on load
-    $('#versions li.older').collapse('hide');
-
     // Package: toggle text in "All Versions / Recent Version" buttons
     $('.show-versions .invisible').removeClass('invisible').toggle();
     $('.show-versions .toggle-text').click(function() {
