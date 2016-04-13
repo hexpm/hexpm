0d49e7c9c3d5160a41b32f62d188fe7303798c45
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2016-04-13 15:03:46 +0200

    Handle invalid ips in logs

--- web/utils.ex
+++ web/utils.ex
@@ -234,12 +234,12 @@ defmodule HexWeb.Utils do
     :public_key.verify(file, :sha512, signature, key)
   end
 
-  def parse_ip("-") do
-    nil
-  end
   def parse_ip(ip) do
-    parts = String.split(ip, ".") |> Enum.map(&String.to_integer/1)
-    for part <- parts, into: <<>>, do: <<part>>
+    parts = String.split(ip, ".")
+    if length(parts) == 4 do
+      parts = Enum.map(parts, &String.to_integer/1)
+      for part <- parts, into: <<>>, do: <<part>>
+    end
   end
 
   defmacro defdispatch({function, _, args}, to: target) do
