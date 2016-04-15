18a6a7f8c291a359c4088c24c57b7b77a837ed03
Ciarán Walsh <git@ciaranwal.sh>
2016-04-15 18:19:26 +0100

    Style the version list expansion as a link rather than a button.

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -28,7 +28,7 @@ export var App = {
     // Package: toggle text in "All Versions / Recent Version" buttons
     $('.show-versions .invisible').removeClass('invisible').toggle();
     $('.show-versions .toggle-text').click(function() {
-      $(this).find('span').toggle();
+      $(this).find('a').toggle();
     });
 
     // Highlight syntax
