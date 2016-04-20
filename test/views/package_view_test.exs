6aeb0b28c27134eac735853eb50a2152feb6b2c0
TakteS <takt96@gmail.com>
2016-04-20 23:31:00 +0900

    Sorting info for default param, relocated tests

---
+++ test/views/package_view_test.exs
@@ -0,0 +1,21 @@
+defmodule HexWeb.PackageViewTest do
+  use HexWeb.ConnCase, async: true
+
+  import Phoenix.View
+  alias HexWeb.PackageView
+
+  test "show sort info" do
+    assert PackageView.show_sort_info("name") == "(Sorted by name)"
+    assert PackageView.show_sort_info("inserted_at") == "(Sorted by recently created)"
+    assert PackageView.show_sort_info("updated_at") == "(Sorted by recently updated)"
+    assert PackageView.show_sort_info("downloads") == "(Sorted by downloads)"
+  end
+
+  test "show sort info when sort param is not available" do
+    assert PackageView.show_sort_info("some param") == nil
+  end
+
+  test "show sort info when sort param is nil" do
+    assert PackageView.show_sort_info(nil) == "(Sorted by name)"
+  end
+end
