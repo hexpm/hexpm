c659f9af0a7757100042694ad72927c8a42a6dd2
TakteS <takt96@gmail.com>
2016-04-20 18:53:08 +0900

    Added sorting info for packages list

--- web/views/package_view.ex
+++ web/views/package_view.ex
@@ -1,3 +1,8 @@
 defmodule HexWeb.PackageView do
   use HexWeb.Web, :view
+
+  def show_sort_info(param) do
+    available_params = %{"name" => "name", "inserted_at" => "recently created", "downloads" => "downloads"}
+    if Map.has_key?(available_params, param), do: "(Sorted by #{available_params[param]})"
+  end
 end
