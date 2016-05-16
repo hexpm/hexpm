fe375ffe6ecf5afb93453370d91ec16e58a44432
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2016-05-16 12:24:21 +0200

    Remove unnecessary mix tasks

--- mix.exs
+++ mix.exs
@@ -42,68 +42,6 @@ defmodule HexWeb.Mixfile do
   defp aliases do
     ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
      "ecto.reset": ["ecto.drop", "ecto.setup"],
-     test: &test/1,
-     "app.start": &app_start/1,
-     "ecto.create": &ecto_create/1,
-     "ecto.drop": &ecto_drop/1,
-     "ecto.migrate": &ecto_migrate/1,
-     "ecto.rollback": &ecto_rollback/1]
-  end
-
-  defp test(args) do
-    env([env: :test, level: :error], fn ->
-      Mix.Task.run "ecto.drop", ["HexWeb.Repo"]
-      Mix.Task.run "ecto.create", ["HexWeb.Repo"]
-      Mix.Task.run "ecto.migrate", ["HexWeb.Repo"]
-      Mix.Task.reenable "app.start"
-      Mix.Task.run "test", args
-    end)
-  end
-
-  defp app_start(args) do
-    Mix.Task.run "app.start", args
-    # Work around bug in 1.0 that stops logger even if --no-start is passed
-    {:ok, _} = Application.ensure_all_started(:logger)
-  end
-
-  defp ecto_create(args) do
-    Mix.Task.run "ecto.create", args ++ ["--no-start"]
-  end
-
-  defp ecto_drop(args) do
-    Mix.Task.run "ecto.drop", args ++ ["--no-start"]
-  end
-
-  defp ecto_migrate(args) do
-    env([level: :warn], fn ->
-      # Workaround for task bug
-      Mix.Task.run "app.start", ["--no-start"]
-      Mix.Task.run "ecto.migrate", args
-    end)
-  end
-
-  defp ecto_rollback(args) do
-    env([level: :warn], fn ->
-      # Workaround for task bug
-      Mix.Task.run "app.start", ["--no-start"]
-      Mix.Task.run "ecto.rollback", args
-    end)
-  end
-
-  defp env(opts, fun) do
-    old_level = Logger.level
-    old_env = Mix.env
-    Logger.configure(level: opts[:level])
-    if opts[:env], do: Mix.env(opts[:env])
-
-    try do
-      fun.()
-    after
-      # If application start fails we need to restart logger because app.start
-      # stops it
-      {:ok, _} = Application.ensure_all_started(:logger)
-      Logger.configure(level: old_level)
-      Mix.env(old_env)
-    end
+     "test": ["ecto.migrate", "test"]]
   end
 end
