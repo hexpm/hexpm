b634edbf965102c343571918d1cfdfbee4147537
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2016-05-21 10:57:39 +0200

    Avoid crashes in normalize_errors

--- web/controllers/controller_helpers.ex
+++ web/controllers/controller_helpers.ex
@@ -73,11 +73,11 @@ defmodule HexWeb.ControllerHelpers do
   # but Hex client expects `{field1: err1, ...}` we normalize to the latter.
   defp normalize_errors(errors) do
     Enum.flat_map(errors, fn
-      {_key, val}   when val == %{}  -> []
-      {_key, [val]} when val == %{}  -> []
-      {key, val}    when is_map(val) -> [{key, normalize_errors(val)}]
-      {key, [val]}  when is_map(val) -> [{key, normalize_errors(val)}]
-      {key, [val]}                   -> [{key, val}]
+      {_key, val}     when val == %{}  -> []
+      {_key, [val|_]} when val == %{}  -> []
+      {key, val}      when is_map(val) -> [{key, normalize_errors(val)}]
+      {key, [val|_]}  when is_map(val) -> [{key, normalize_errors(val)}]
+      {key, [val|_]}                   -> [{key, val}]
     end)
     |> Enum.into(%{})
   end
