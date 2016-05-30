ce6fe5f38b6f011c686b7b4e8b53a3a47a585981
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2016-05-30 12:37:21 +0200

    Add missing timestamp indicies

---
+++ priv/repo/migrations/20160530102429_add_missing_timestamp_indicies_to_packages_and_releases.exs
@@ -0,0 +1,15 @@
+defmodule HexWeb.Repo.Migrations.AddimestampIndiciesToPackagesAndReleases do
+  use Ecto.Migration
+
+  def up do
+    execute "CREATE INDEX ON packages (inserted_at)"
+    execute "CREATE INDEX ON packages (updated_at)"
+    execute "CREATE INDEX ON releases (inserted_at)"
+  end
+
+  def down do
+    execute "DROP INDEX packages_inserted_at_idx"
+    execute "DROP INDEX packages_updated_at_idx"
+    execute "DROP INDEX releases_inserted_at_idx"
+  end
+end
