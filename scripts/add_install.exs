c96cc3b1b3aba47df7643ee8ca1f408d8c9fed0d
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2016-06-09 18:06:26 +0300

    Add to list.csv when adding Hex installs

--- scripts/add_install.exs
+++ scripts/add_install.exs
@@ -1,5 +1,3 @@
-HexWeb.CDN.purge_key(:fastly_hexrepo, "installs")
-
 case System.argv do
   [hex | elixirs] ->
     IO.puts "Hex:     " <> hex
@@ -17,6 +15,12 @@ csv =
   Enum.map_join(all, "\n", fn install ->
     Enum.join([install.hex|install.elixirs], ",")
   end)
+
+opts = [acl: :public_read, content_type: "text/csv", cache_control: "public, max-age=604800", meta: [{"surrogate-key", "installs"}]]
+HexWeb.Store.S3.put(nil, :s3_bucket, "installs/list.csv", csv, opts)
+HexWeb.CDN.purge_key(:fastly_hexrepo, "installs")
+
+IO.puts "Uploaded installs/list.csv"
 
 HexWeb.RegistryBuilder.rebuild
 
