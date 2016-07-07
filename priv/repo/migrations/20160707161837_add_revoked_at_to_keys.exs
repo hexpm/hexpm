5b33dd82b53e84be70aef45e3b043bc5ee3ba8c6
Andrew Bennett <potatosaladx@gmail.com>
2016-07-07 16:53:59 -0600

    Remove tabs accidentally included in migration file.

--- priv/repo/migrations/20160707161837_add_revoked_at_to_keys.exs
+++ priv/repo/migrations/20160707161837_add_revoked_at_to_keys.exs
@@ -10,10 +10,10 @@ defmodule HexWeb.Repo.Migrations.AddRevokedAtToKeys do
   end
 
   def down do
-  	execute "ALTER TABLE keys DROP CONSTRAINT keys_user_id_name_revoked_at_key"
-  	execute "ALTER TABLE keys ADD CONSTRAINT keys_user_id_name_key UNIQUE (user_id, name)"
-  	alter table(:keys) do
-  	  remove :revoked_at
-  	end
+    execute "ALTER TABLE keys DROP CONSTRAINT keys_user_id_name_revoked_at_key"
+    execute "ALTER TABLE keys ADD CONSTRAINT keys_user_id_name_key UNIQUE (user_id, name)"
+    alter table(:keys) do
+      remove :revoked_at
+    end
   end
 end
