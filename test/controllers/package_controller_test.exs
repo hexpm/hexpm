45a92c301687324b94a589fb654439ada7106628
Wojtek Mach <wojtekmach@users.noreply.github.com>
2016-09-27 20:05:57 +0200

    Break out Repo access from PackageController (#411)

---
+++ test/controllers/package_controller_test.exs
@@ -0,0 +1,56 @@
+defmodule HexWeb.PackageControllerTest do
+  use HexWeb.ConnCase, async: true
+  alias HexWeb.{User, Package, Release}
+
+  setup do
+    eric = User.build(%{username: "eric", email: "eric@mail.com", password: "eric"}, true) |> HexWeb.Repo.insert!
+    decimal = Package.build(eric, pkg_meta(%{name: "decimal", description: "Arbitrary precision decimal arithmetic for Elixir."})) |> HexWeb.Repo.insert!
+    Package.build(eric, pkg_meta(%{name: "postgrex", description: "Postgrex is awesome"})) |> HexWeb.Repo.insert!
+    Release.build(decimal, rel_meta(%{version: "0.0.1", app: "decimal"}), "") |> HexWeb.Repo.insert!
+    Release.build(decimal, rel_meta(%{version: "0.0.2", app: "decimal"}), "") |> HexWeb.Repo.insert!
+    :ok
+  end
+
+  test "index" do
+    conn = get build_conn(), "/packages"
+    assert conn.status == 200
+    assert conn.resp_body =~ ~r/decimal.*0.0.2/
+    assert conn.resp_body =~ ~r/postgrex/
+  end
+
+  test "index with letter" do
+    conn = get build_conn(), "/packages?letter=D"
+    assert conn.status == 200
+    assert conn.resp_body =~ ~r/decimal/
+    refute conn.resp_body =~ ~r/postgrex/
+
+    conn = get build_conn(), "/packages?letter=P"
+    assert conn.status == 200
+    refute conn.resp_body =~ ~r/decimal/
+    assert conn.resp_body =~ ~r/postgrex/
+  end
+
+  test "index with search query" do
+    conn = get build_conn(), "/packages?search=dec"
+    assert conn.status == 200
+    assert conn.resp_body =~ ~r/decimal.*0.0.2/
+    refute conn.resp_body =~ ~r/postgrex/
+  end
+
+  test "show package" do
+    conn = get build_conn(), "/packages/decimal"
+    assert conn.status == 200
+    assert String.contains?(conn.resp_body, escape("{:decimal, \"~> 0.0.2\"}"))
+  end
+
+  test "show package version" do
+    conn = get build_conn(), "/packages/decimal/0.0.1"
+    assert conn.status == 200
+    assert String.contains?(conn.resp_body, escape("{:decimal, \"~> 0.0.1\"}"))
+  end
+
+  defp escape(html) do
+    {:safe, safe} = Phoenix.HTML.html_escape(html)
+    safe
+  end
+end
--- web/models/packages.ex
+++ web/models/packages.ex
@@ -1,6 +1,10 @@
 defmodule HexWeb.Packages do
   use HexWeb.Web, :crud
 
+  def count(filter) do
+     HexWeb.Repo.one!(Package.count(filter))
+  end
+
   def get(name) do
     Repo.get_by!(Package, name: name)
   end
@@ -13,12 +17,24 @@ defmodule HexWeb.Packages do
     update_in(package.releases, &Release.sort/1)
   end
 
-  def search(page, query, sort) do
-    Package.all(page, 100, query, sort)
+  def search(page, packages_per_page, query, sort) do
+    Package.all(page, packages_per_page, query, sort)
     |> Ecto.Query.preload(releases: ^from(r in Release, select: map(r, [:version])))
     |> Repo.all
     |> Enum.map(fn package ->
       update_in(package.releases, &Release.sort/1)
     end)
+  end
+
+  def package_downloads(package) do
+    PackageDownload.package(package)
+    |> Repo.all
+    |> Enum.into(%{})
+  end
+
+  def packages_downloads(packages, view) do
+    PackageDownload.packages(packages, view)
+    |> Repo.all
+    |> Enum.into(%{})
   end
 end
