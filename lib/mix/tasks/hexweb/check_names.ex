robottokauf3/hex_web

Use Bamboo to deliver emails

lib/mix/tasks/hexweb/check_names.ex
@@ -11,7 +11,8 @@ defmodule Mix.Tasks.Hexweb.CheckNames do
 
     threshold
     |> find_candidates
-    |> HexWeb.Mailer.send_typosquat_candidates_email(threshold)
+    |> HexWeb.Email.typosquat_candidates(threshold)
+    |> HexWeb.Mailer.deliver_now
 
     :ok
   end

web/models/owners.ex
@@ -16,7 +16,8 @@ defmodule HexWeb.Owners do
       {:ok, _} ->
         owners = package |> all |> Users.with_emails
         owner = Enum.find(owners, &(&1.id == owner.id))
-        Mailer.send_owner_added_email(package, owners, owner)
+
+        Email.owner_added(package, owners, owner) |> Mailer.deliver_now
         :ok
       {:error, :owner, changeset, _} ->
         {:error, changeset}
@@ -36,7 +37,7 @@ defmodule HexWeb.Owners do
 
       {:ok, _} = Repo.transaction(multi)
       owner = Enum.find(owners, &(&1.id == owner.id))
-      Mailer.send_owner_removed_email(package, owners, owner)
+      Email.owner_removed(package, owners, owner) |> Mailer.deliver_now
       :ok
     end
   end

hexpm/hexpm

lib/mix/tasks/hexweb/check_names.ex
@@ -11,7 +11,8 @@ defmodule Mix.Tasks.Hexweb.CheckNames do
 
     threshold
     |> find_candidates
-    |> HexWeb.Mailer.send_typosquat_candidates_email(threshold)
+    |> HexWeb.Mail.typosquat_candidates(threshold)
+    |> HexWeb.Mailer.deliver_now_throttled
 
     :ok
   end

web/models/owners.ex
@@ -16,7 +16,7 @@ defmodule HexWeb.Owners do
       {:ok, _} ->
         owners = package |> all |> Users.with_emails
         owner = Enum.find(owners, &(&1.id == owner.id))
-        Mailer.send_owner_added_email(package, owners, owner)
+        Mail.owner_added(package, owners, owner) |> Mailer.deliver_now_throttled
         :ok
       {:error, :owner, changeset, _} ->
         {:error, changeset}
@@ -36,7 +36,7 @@ defmodule HexWeb.Owners do
 
       {:ok, _} = Repo.transaction(multi)
       owner = Enum.find(owners, &(&1.id == owner.id))
-      Mailer.send_owner_removed_email(package, owners, owner)
+      Mail.owner_removed(package, owners, owner) |> Mailer.deliver_now_throttled
       :ok
     end
   end