bb2047aba292b915a18b9b980298fcf55b00d10e
Mathieu Rhéaume <mathieu@codingrhemes.com>
2017-01-03 20:09:06 -0500

    Improve test coverage on messages for error view (#462)
    - Assert the error messages for error code 400, 408, 413, 415 and 422.

--- test/views/error_view_test.exs
+++ test/views/error_view_test.exs
@@ -4,9 +4,34 @@ defmodule HexWeb.ErrorViewTest do
 
   import Phoenix.View
 
+  test "renders 400.html" do
+    assert render_to_string(HexWeb.ErrorView, "400.html", conn: conn()) =~
+           "Bad request"
+  end
+
   test "renders 404.html" do
     assert render_to_string(HexWeb.ErrorView, "404.html", conn: conn()) =~
            "Page not found"
+  end
+
+  test "renders 408.html" do
+    assert render_to_string(HexWeb.ErrorView, "408.html", conn: conn()) =~
+           "Request timeout"
+  end
+
+  test "renders 413.html" do
+    assert render_to_string(HexWeb.ErrorView, "413.html", conn: conn()) =~
+           "Payload too large"
+  end
+
+  test "renders 415.html" do
+    assert render_to_string(HexWeb.ErrorView, "415.html", conn: conn()) =~
+           "Unsupported media type"
+  end
+
+  test "renders 422.html" do
+    assert render_to_string(HexWeb.ErrorView, "422.html", conn: conn()) =~
+           "Validation error(s)"
   end
 
   test "render 500.html" do
