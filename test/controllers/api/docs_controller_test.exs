3bea914622b990048cdfff09cf22526a1b480a32
Wojtek Mach <wojtek@wojtekmach.pl>
2017-01-14 00:42:38 +0100

    Refactor HexWeb.API.DocsControllerTest

--- test/controllers/api/docs_controller_test.exs
+++ test/controllers/api/docs_controller_test.exs
@@ -1,7 +1,6 @@
 defmodule HexWeb.API.DocsControllerTest do
   use HexWeb.ConnCase, async: true
 
-  alias HexWeb.User
   alias HexWeb.Package
   alias HexWeb.Release
 
@@ -11,17 +10,17 @@ defmodule HexWeb.API.DocsControllerTest do
   end
 
   @tag :integration
-  test "release docs", %{user: user} do
-    phoenix = Package.build(user, pkg_meta(%{name: "phoenix", description: "Web framework"})) |> HexWeb.Repo.insert!
+  test "release docs", c do
+    phoenix = Package.build(c.user, pkg_meta(%{name: "phoenix", description: "Web framework"})) |> HexWeb.Repo.insert!
     Release.build(phoenix, rel_meta(%{version: "0.0.1", app: "phoenix"}), "") |> HexWeb.Repo.insert!
     Release.build(phoenix, rel_meta(%{version: "0.5.0", app: "phoenix"}), "") |> HexWeb.Repo.insert!
 
-    conn = publish_docs(user, phoenix, "0.0.1", [{'index.html', "phoenix v0.0.1"}])
+    conn = publish_docs(c.user, phoenix, "0.0.1", [{'index.html', "phoenix v0.0.1"}])
     assert conn.status == 201
     assert HexWeb.Repo.get_by!(assoc(phoenix, :releases), version: "0.0.1").has_docs
 
     log = HexWeb.Repo.one!(HexWeb.AuditLog)
-    assert log.actor_id == user.id
+    assert log.actor_id == c.user.id
     assert log.action == "docs.publish"
     assert %{"package" => %{"name" => "phoenix"}, "release" => %{"version" => "0.0.1"}} = log.params
 
@@ -31,7 +30,7 @@ defmodule HexWeb.API.DocsControllerTest do
     conn = get build_conn(), "docs/phoenix/0.0.1/index.html"
     assert response(conn, 200) == "phoenix v0.0.1"
 
-    conn = publish_docs(user, phoenix, "0.5.0", [{'index.html', "phoenix v0.5.0"}])
+    conn = publish_docs(c.user, phoenix, "0.5.0", [{'index.html', "phoenix v0.5.0"}])
     assert conn.status == 201
 
     conn = get build_conn(), "docs/phoenix/index.html"
@@ -43,7 +42,7 @@ defmodule HexWeb.API.DocsControllerTest do
     conn = get build_conn(), "docs/sitemap.xml"
     assert response(conn, 200) =~ "https://hexdocs.pm/phoenix"
 
-    conn = publish_docs(user, phoenix, "0.0.1", [{'index.html', "phoenix v0.0.1 (updated)"}])
+    conn = publish_docs(c.user, phoenix, "0.0.1", [{'index.html', "phoenix v0.0.1 (updated)"}])
     assert conn.status == 201
 
     conn = get build_conn(), "docs/phoenix/index.html"
@@ -54,26 +53,26 @@ defmodule HexWeb.API.DocsControllerTest do
   end
 
   @tag :integration
-  test "release beta docs", %{user: user} do
-    plug = Package.build(user, pkg_meta(%{name: "plug", description: "Web framework"})) |> HexWeb.Repo.insert!
+  test "release beta docs", c do
+    plug = Package.build(c.user, pkg_meta(%{name: "plug", description: "Web framework"})) |> HexWeb.Repo.insert!
     Release.build(plug, rel_meta(%{version: "0.0.1-beta.1", app: "plug"}), "") |> HexWeb.Repo.insert!
     Release.build(plug, rel_meta(%{version: "0.5.0", app: "plug"}), "") |> HexWeb.Repo.insert!
     Release.build(plug, rel_meta(%{version: "1.0.0-beta.1", app: "plug"}), "") |> HexWeb.Repo.insert!
 
-    conn = publish_docs(user, plug, "0.0.1-beta.1", [{'index.html', "plug v0.0.1-beta.1"}])
+    conn = publish_docs(c.user, plug, "0.0.1-beta.1", [{'index.html', "plug v0.0.1-beta.1"}])
     assert conn.status == 201
     assert HexWeb.Repo.get_by!(assoc(plug, :releases), version: "0.0.1-beta.1").has_docs
 
     conn = get build_conn(), "docs/plug/index.html"
     assert response(conn, 200) == "plug v0.0.1-beta.1"
 
-    conn = publish_docs(user, plug, "0.5.0", [{'index.html', "plug v0.5.0"}])
+    conn = publish_docs(c.user, plug, "0.5.0", [{'index.html', "plug v0.5.0"}])
     assert conn.status == 201
 
     conn = get build_conn(), "docs/plug/index.html"
     assert response(conn, 200) == "plug v0.5.0"
 
-    conn = publish_docs(user, plug, "1.0.0-beta.1", [{'index.html', "plug v1.0.0-beta.1"}])
+    conn = publish_docs(c.user, plug, "1.0.0-beta.1", [{'index.html', "plug v1.0.0-beta.1"}])
     assert conn.status == 201
 
     conn = get build_conn(), "docs/plug/index.html"
@@ -84,16 +83,15 @@ defmodule HexWeb.API.DocsControllerTest do
   end
 
   @tag isolation: :serializable
-  test "delete release with docs" do
-    user = HexWeb.Repo.get_by!(User, username: "eric")
-    ecto = Package.build(user, pkg_meta(%{name: "ecto", description: "DSL"})) |> HexWeb.Repo.insert!
+  test "delete release with docs", c do
+    ecto = Package.build(c.user, pkg_meta(%{name: "ecto", description: "DSL"})) |> HexWeb.Repo.insert!
     Release.build(ecto, rel_meta(%{version: "0.0.1", app: "ecto"}), "") |> HexWeb.Repo.insert!
 
-    conn = publish_docs(user, ecto, "0.0.1", [{'index.html', "ecto v0.0.1"}])
+    conn = publish_docs(c.user, ecto, "0.0.1", [{'index.html', "ecto v0.0.1"}])
     assert conn.status == 201
     assert HexWeb.Repo.get_by!(assoc(ecto, :releases), version: "0.0.1").has_docs
 
-    conn = revert_release(user, ecto, "0.0.1")
+    conn = revert_release(c.user, ecto, "0.0.1")
     assert conn.status == 204
 
     # Check release was deleted
@@ -108,21 +106,21 @@ defmodule HexWeb.API.DocsControllerTest do
   end
 
   @tag :integration
-  test "delete docs", %{user: user} do
-    ecto = Package.build(user, pkg_meta(%{name: "ecto", description: "DSL"})) |> HexWeb.Repo.insert!
+  test "delete docs", c do
+    ecto = Package.build(c.user, pkg_meta(%{name: "ecto", description: "DSL"})) |> HexWeb.Repo.insert!
     Release.build(ecto, rel_meta(%{version: "0.0.1", app: "ecto"}), "") |> HexWeb.Repo.insert!
     Release.build(ecto, rel_meta(%{version: "0.5.0", app: "ecto"}), "") |> HexWeb.Repo.insert!
     Release.build(ecto, rel_meta(%{version: "2.0.0", app: "ecto"}), "") |> HexWeb.Repo.insert!
 
-    conn = publish_docs(user, ecto, "0.0.1", [{'index.html', "ecto v0.0.1"}])
+    conn = publish_docs(c.user, ecto, "0.0.1", [{'index.html', "ecto v0.0.1"}])
     assert conn.status == 201
-    conn = publish_docs(user, ecto, "0.5.0", [{'index.html', "ecto v0.5.0"}])
+    conn = publish_docs(c.user, ecto, "0.5.0", [{'index.html', "ecto v0.5.0"}])
     assert conn.status == 201
-    conn = publish_docs(user, ecto, "2.0.0", [{'index.html', "ecto v2.0.0"}])
+    conn = publish_docs(c.user, ecto, "2.0.0", [{'index.html', "ecto v2.0.0"}])
     assert conn.status == 201
 
     # Revert middle release
-    conn = revert_docs(user, ecto, "0.5.0")
+    conn = revert_docs(c.user, ecto, "0.5.0")
     assert conn.status == 204
 
     # Check release was deleted
@@ -130,7 +128,7 @@ defmodule HexWeb.API.DocsControllerTest do
 
     [%{action: "docs.publish"}, %{action: "docs.publish"}, %{action: "docs.publish"}, log] =
       HexWeb.Repo.all(HexWeb.AuditLog)
-    assert log.actor_id == user.id
+    assert log.actor_id == c.user.id
     assert log.action == "docs.revert"
     assert %{"package" => %{"name" => "ecto"}, "release" => %{"version" => "0.5.0"}} = log.params
 
@@ -145,7 +143,7 @@ defmodule HexWeb.API.DocsControllerTest do
     assert response(conn, 200) == "ecto v2.0.0"
 
     # Revert latest release
-    conn = revert_docs(user, ecto, "2.0.0")
+    conn = revert_docs(c.user, ecto, "2.0.0")
     assert conn.status == 204
 
     # Check release was deleted
@@ -163,7 +161,7 @@ defmodule HexWeb.API.DocsControllerTest do
     assert response(conn, 200) == "ecto v2.0.0"
 
     # Revert remaining release
-    conn = revert_docs(user, ecto, "0.0.1")
+    conn = revert_docs(c.user, ecto, "0.0.1")
     assert conn.status == 204
 
     # Check release was deleted
@@ -182,11 +180,11 @@ defmodule HexWeb.API.DocsControllerTest do
   end
 
   @tag :integration
-  test "dont allow version directories in docs", %{user: user} do
-    ecto = Package.build(user, pkg_meta(%{name: "ecto", description: "DSL"})) |> HexWeb.Repo.insert!
+  test "dont allow version directories in docs", c do
+    ecto = Package.build(c.user, pkg_meta(%{name: "ecto", description: "DSL"})) |> HexWeb.Repo.insert!
     Release.build(ecto, rel_meta(%{version: "0.0.1", app: "ecto"}), "") |> HexWeb.Repo.insert!
 
-    conn = publish_docs(user, ecto, "0.0.1", [{'1.2.3', "ecto v0.0.1"}])
+    conn = publish_docs(c.user, ecto, "0.0.1", [{'1.2.3', "ecto v0.0.1"}])
     assert conn.status == 422
     assert %{"errors" => %{"tar" => "directory name not allowed to match a semver version"}} =
            Poison.decode!(conn.resp_body)
