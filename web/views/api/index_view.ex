dabdf3597b3b29b364ef06ff595b17fc1c107f4b
Wojtek Mach <wojtek@wojtekmach.pl>
2017-01-15 13:19:00 +0100

    Add remaining endpoints

--- web/views/api/index_view.ex
+++ web/views/api/index_view.ex
@@ -3,6 +3,11 @@ defmodule HexWeb.API.IndexView do
 
   def render("index." <> _format, _assigns) do
     %{packages_url: api_package_url(Endpoint, :index),
+      package_url: api_package_url(Endpoint, :show, "{name}"),
+      package_release_url: api_release_url(Endpoint, :show, "{name}", "{version}"),
+      package_owners_url: api_owner_url(Endpoint, :index, "{name}"),
+      keys_url: api_key_url(Endpoint, :index),
+      key_url: api_key_url(Endpoint, :show, "{name}"),
       documentation_url: "http://docs.hexpm.apiary.io"}
   end
 end
