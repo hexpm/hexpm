28541522a55119639c4e0435ee32e03476485402
Wojtek Mach <wojtek@wojtekmach.pl>
2017-01-16 20:37:18 +0100

    Better function name

--- web/views/api/index_view.ex
+++ web/views/api/index_view.ex
@@ -3,15 +3,15 @@ defmodule HexWeb.API.IndexView do
 
   def render("index." <> _format, _assigns) do
     %{packages_url: api_package_url(Endpoint, :index),
-      package_url: api_package_url(Endpoint, :show, "{name}") |> placeholder(),
-      package_release_url: api_release_url(Endpoint, :show, "{name}", "{version}") |> placeholder(),
-      package_owners_url: api_owner_url(Endpoint, :index, "{name}") |> placeholder(),
+      package_url: api_package_url(Endpoint, :show, "{name}") |> fix_placeholder(),
+      package_release_url: api_release_url(Endpoint, :show, "{name}", "{version}") |> fix_placeholder(),
+      package_owners_url: api_owner_url(Endpoint, :index, "{name}") |> fix_placeholder(),
       keys_url: api_key_url(Endpoint, :index),
-      key_url: api_key_url(Endpoint, :show, "{name}") |> placeholder(),
+      key_url: api_key_url(Endpoint, :show, "{name}") |> fix_placeholder(),
       documentation_url: "http://docs.hexpm.apiary.io"}
   end
 
-  defp placeholder(url) do
+  defp fix_placeholder(url) do
     url
     |> String.replace("%7B", "{")
     |> String.replace("%7D", "}")
