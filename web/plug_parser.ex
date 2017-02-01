bee55f530266a533b12ca90bffcc95f44868cc16
Imran Ismail <imran.codely@gmail.com>
2017-02-02 01:20:42 +0800

    Refactor plug_parser.ex

--- web/plug_parser.ex
+++ web/plug_parser.ex
@@ -5,58 +5,48 @@ defmodule HexWeb.PlugParser do
 
   def parse(%Conn{} = conn, "application", "vnd.hex+" <> format, _headers, opts)
       when format in @formats do
-
+    decoder = get_decoder(format, opts)
     conn
     |> Conn.read_body(opts)
-    |> decode(format)
+    |> decode(decoder)
   end
 
   def parse(conn, _type, _subtype, _headers, _opts) do
     {:next, conn}
   end
 
-  defp decode({:more, _, conn}, _format) do
+  defp decode({:more, _, conn}, _decoder) do
     {:error, :too_large, conn}
   end
 
-  defp decode({:ok, "", conn}, _format) do
+  defp decode({:error, :timeout}, _decoder) do
+    raise Plug.TimeoutError
+  end
+
+  defp decode({:error, _}, _decoder) do
+    raise Plug.BadRequestError
+  end
+
+  defp decode({:ok, "", conn}, _decoder) do
     {:ok, %{}, conn}
   end
 
-  defp decode({:ok, body, conn}, "elixir") do
-    case HexWeb.ElixirFormat.decode(body) do
-      {:ok, terms} when is_map(terms) ->
-        {:ok, terms, conn}
-      {:ok, terms} ->
-        {:ok, %{"_json" => terms}, conn}
-      {:error, reason} ->
-        raise HexWeb.Plugs.BadRequestError, message: reason
-    end
-  end
-
-  defp decode({:ok, body, conn}, "erlang") do
-    case HexWeb.ErlangFormat.decode(body) do
-      {:ok, terms} when is_map(terms) ->
-        {:ok, terms, conn}
-      {:ok, terms} ->
-        {:ok, %{"_json" => terms}, conn}
-      {:error, reason} ->
-        raise HexWeb.Plugs.BadRequestError, message: reason
-    end
-  end
-
-  defp decode({:ok, body, conn}, "json") do
-    decoder = 
-      Keyword.get(opts, :json_decoder) ||
-      raise ArgumentError, "PlugParser parser expects a :json_decoder option"
-      
+  defp decode({:ok, body, conn}, decoder) do
     case decoder.decode(body) do
       {:ok, terms} when is_map(terms) ->
         {:ok, terms, conn}
       {:ok, terms} ->
         {:ok, %{"_json" => terms}, conn}
-      _ ->
-        raise HexWeb.Plugs.BadRequestError, message: "malformed JSON"
+      {:error, reason} ->
+        raise Plug.BadRequestError, message: reason
+    end
+  end
+
+  defp get_decoder(format, opts) do
+    case format do
+      "elixir" -> HexWeb.ElixirFormat
+      "erlang" -> HexWeb.ErlangFormat
+      "json" -> Keyword.fetch!(opts, :json_decoder)
     end
   end
 end
