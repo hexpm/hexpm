a81e8c57160f97bbbfd6e5478662120a1b9ec369
Mathieu Rheaume <mathieu@codingrhemes.com>
2017-02-19 22:23:42 -0500

    Improve test coverage on opensearch controller

---
+++ test/hexpm/web/controllers/opensearch_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule Hexpm.Web.OpenSearchControllerTest do
+  use Hexpm.ConnCase, async: true
+
+  test "opensearch" do
+    conn = get build_conn(), "/hexsearch.xml"
+    assert response(conn, 200) =~ "<Url type=\"text/html\" method=\"get\" template=\"http://localhost:4001/packages?search={searchTerms}&amp;sort=downloads\" />"
+  end
+end
