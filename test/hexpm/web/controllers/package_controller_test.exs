f643638c4a7f7ed45527140e93c56b50dc881c8d
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2017-03-13 00:13:35 +0100

    Fix up PackageController tests

--- test/hexpm/web/controllers/package_controller_test.exs
+++ test/hexpm/web/controllers/package_controller_test.exs
@@ -1,51 +1,55 @@
 defmodule Hexpm.Web.PackageControllerTest do
   use Hexpm.ConnCase, async: true
-  alias Hexpm.Repository.{Package, Release}
 
   setup do
-    eric = create_user("eric", "eric@mail.com", "ericeric")
-    decimal = Package.build(eric, pkg_meta(%{name: "decimal", description: "Arbitrary precision decimal arithmetic for Elixir."})) |> Hexpm.Repo.insert!
-    Package.build(eric, pkg_meta(%{name: "postgrex", description: "Postgrex is awesome"})) |> Hexpm.Repo.insert!
-    Release.build(decimal, rel_meta(%{version: "0.0.1", app: "decimal"}), "") |> Hexpm.Repo.insert!
-    Release.build(decimal, rel_meta(%{version: "0.0.2", app: "decimal"}), "") |> Hexpm.Repo.insert!
-    Release.build(decimal, rel_meta(%{version: "0.0.3-dev", app: "decimal"}), "") |> Hexpm.Repo.insert!
-    :ok
+    package1 = insert(:package, name: "dec_packagecontroller")
+    package2 = insert(:package, name: "post_packagecontroller")
+    insert(:release, package: package1, version: "0.0.1", meta: build(:release_metadata, app: "dec_packagecontroller"))
+    insert(:release, package: package1, version: "0.0.2", meta: build(:release_metadata, app: "dec_packagecontroller"))
+    insert(:release, package: package1, version: "0.0.3-dev", meta: build(:release_metadata, app: "dec_packagecontroller"))
+    %{package1: package1, package2: package2}
   end
 
-  test "index" do
-    conn = get build_conn(), "/packages"
-    assert conn.status == 200
-    assert conn.resp_body =~ ~r/decimal.*0.0.2/
-    assert conn.resp_body =~ ~r/postgrex/
+  describe "GET /packages" do
+    test "index" do
+      conn = get build_conn(), "/packages"
+      assert conn.status == 200
+      assert conn.resp_body =~ ~r/dec_packagecontroller.*0.0.2/
+      assert conn.resp_body =~ ~r/post_packagecontroller/
+    end
+
+    test "index with letter" do
+      conn = get build_conn(), "/packages?letter=D"
+      assert conn.status == 200
+      assert conn.resp_body =~ ~r/dec_packagecontroller/
+      refute conn.resp_body =~ ~r/post_packagecontroller/
+
+      conn = get build_conn(), "/packages?letter=P"
+      assert conn.status == 200
+      refute conn.resp_body =~ ~r/dec_packagecontroller/
+      assert conn.resp_body =~ ~r/post_packagecontroller/
+    end
+
+    test "index with search query" do
+      conn = get build_conn(), "/packages?search=dec"
+      assert conn.status == 200
+      assert conn.resp_body =~ ~r/dec_packagecontroller.*0.0.2/
+      refute conn.resp_body =~ ~r/post_packagecontroller/
+    end
   end
 
-  test "index with letter" do
-    conn = get build_conn(), "/packages?letter=D"
-    assert conn.status == 200
-    assert conn.resp_body =~ ~r/decimal/
-    refute conn.resp_body =~ ~r/postgrex/
-
-    conn = get build_conn(), "/packages?letter=P"
-    assert conn.status == 200
-    refute conn.resp_body =~ ~r/decimal/
-    assert conn.resp_body =~ ~r/postgrex/
+  describe "GET /packages/:name" do
+    test "show package" do
+      conn = get build_conn(), "/packages/dec_packagecontroller"
+      assert response(conn, 200) =~ escape(~s({:dec_packagecontroller, "~> 0.0.2"}))
+    end
   end
 
-  test "index with search query" do
-    conn = get build_conn(), "/packages?search=dec"
-    assert conn.status == 200
-    assert conn.resp_body =~ ~r/decimal.*0.0.2/
-    refute conn.resp_body =~ ~r/postgrex/
-  end
-
-  test "show package" do
-    conn = get build_conn(), "/packages/decimal"
-    assert response(conn, 200) =~ escape("{:decimal, \"~> 0.0.2\"}")
-  end
-
-  test "show package version" do
-    conn = get build_conn(), "/packages/decimal/0.0.1"
-    assert response(conn, 200) =~ escape("{:decimal, \"~> 0.0.1\"}")
+  describe "GET /packages/:name/:version" do
+    test "show package version" do
+      conn = get build_conn(), "/packages/dec_packagecontroller/0.0.1"
+      assert response(conn, 200) =~ escape(~s({:dec_packagecontroller, "~> 0.0.1"}))
+    end
   end
 
   defp escape(html) do
