da9ecea83f994871013d9c7d052e74d8002a0199
Wojtek Mach <wojtek@wojtekmach.pl>
2017-03-18 22:29:06 +0100

    Fix up PasswordController tests

--- test/hexpm/web/controllers/password_controller_test.exs
+++ test/hexpm/web/controllers/password_controller_test.exs
@@ -5,41 +5,47 @@ defmodule Hexpm.Web.PasswordControllerTest do
   alias Hexpm.Accounts.Users
 
   setup do
-    %{user: create_user("eric", "eric@mail.com", "hunter42")}
+    user = insert(:user, password: Auth.gen_password("hunter42"))
+    %{user: user}
   end
 
-  test "show select new password redirect" do
-    conn = get(build_conn(), "password/new", %{"username" => "username", "key" => "RESET_KEY"})
+  describe "GET /password/new" do
+    test "show select new password redirect" do
+      conn = get(build_conn(), "password/new", %{"username" => "username", "key" => "RESET_KEY"})
 
-    assert redirected_to(conn) == "/password/new"
-    assert conn.resp_cookies["reset_username"][:value] == "username"
-    assert conn.resp_cookies["reset_key"][:value] == "RESET_KEY"
+      assert redirected_to(conn) == "/password/new"
+      assert conn.resp_cookies["reset_username"][:value] == "username"
+      assert conn.resp_cookies["reset_key"][:value] == "RESET_KEY"
+    end
+
+    test "show select new password" do
+      conn = build_conn()
+             |> put_req_cookie("reset_username", "username")
+             |> put_req_cookie("reset_key", "RESET_KEY")
+             |> get("password/new")
+
+      assert conn.status == 200
+      assert conn.resp_body =~ "Choose a new password"
+      assert conn.resp_body =~ "RESET_KEY"
+    end
   end
 
-  test "show select new password" do
-    conn = build_conn()
-           |> put_req_cookie("reset_username", "username")
-           |> put_req_cookie("reset_key", "RESET_KEY")
-           |> get("password/new")
+  describe "POST /password/new" do
+    test "submit new password", c do
+      username = c.user.username
+      assert {:ok, {%User{username: ^username}, _, _}} = Auth.password_auth(username, "hunter42")
 
-    assert conn.status == 200
-    assert conn.resp_body =~ "Choose a new password"
-    assert conn.resp_body =~ "RESET_KEY"
-  end
+      # initiate password reset (usually done via api)
+      user = User.init_password_reset(c.user) |> Hexpm.Repo.update!
+      user = Users.sign_in(user)
 
-  test "submit new password", c do
-    assert {:ok, {%User{username: "eric"}, _, _}} = Auth.password_auth("eric", "hunter42")
+      # chose new password (using token) to `abcd1234`
+      conn = post(build_conn(), "password/new", %{"user" => %{"username" => user.username, "key" => user.reset_key, "password" => "abcd1234"}})
+      assert redirected_to(conn) == "/"
+      assert get_flash(conn, :info) =~ "password has been changed"
 
-    # initiate password reset (usually done via api)
-    user = User.init_password_reset(c.user) |> Hexpm.Repo.update!
-    user = Users.sign_in(user)
-
-    # chose new password (using token) to `abcd1234`
-    conn = post(build_conn(), "password/new", %{"user" => %{"username" => user.username, "key" => user.reset_key, "password" => "abcd1234"}})
-    assert redirected_to(conn) == "/"
-    assert get_flash(conn, :info) =~ "password has been changed"
-
-    # check new password will work
-    assert {:ok, {%User{username: "eric", session_key: nil}, _, _}} = Auth.password_auth("eric", "abcd1234")
+      # check new password will work
+      assert {:ok, {%User{username: ^username, session_key: nil}, _, _}} = Auth.password_auth(username, "abcd1234")
+    end
   end
 end
