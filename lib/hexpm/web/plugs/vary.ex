60660a277bcc55caf97a9adea11545f6523e379f
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2017-05-26 23:29:49 +0200

    Compress responses

---
+++ lib/hexpm/web/plugs/vary.ex
@@ -0,0 +1,13 @@
+defmodule Hexpm.Web.Plugs.Vary do
+  import Plug.Conn
+
+  def init(opts), do: opts
+
+  def call(conn, vary) do
+    register_before_send(conn, fn conn ->
+      original_vary = get_resp_header(conn, "vary")
+      vary = Enum.join(original_vary ++ vary, ", ")
+      put_resp_header(conn, "vary", vary)
+    end)
+  end
+end
