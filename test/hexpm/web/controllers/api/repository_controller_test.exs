11c350724a94186fff7ae331a19de8ce9d75ca04
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2017-07-18 19:11:50 +0200

    Change route /api/repositories => /api/repos

--- test/hexpm/web/controllers/api/repository_controller_test.exs
+++ test/hexpm/web/controllers/api/repository_controller_test.exs
@@ -6,13 +6,13 @@ defmodule Hexpm.Web.API.RepositoryControllerTest do
     repository1 = insert(:repository, public: true)
     repository2 = insert(:repository, public: false)
     insert(:repository, public: false)
-    insert(:repostory_user, user: user, repository: repository2)
+    insert(:repository_user, user: user, repository: repository2)
     %{user: user, repository1: repository1, repository2: repository2}
   end
 
-  describe "GET /api/repositories" do
+  describe "GET /api/repos" do
     test "not authorized" do
-      conn = get build_conn(), "api/repositories"
+      conn = get build_conn(), "api/repos"
       result = json_response(conn, 200)
       assert length(result) == 2
     end
@@ -21,19 +21,19 @@ defmodule Hexpm.Web.API.RepositoryControllerTest do
       result =
         build_conn()
         |> put_req_header("authorization", key_for(user))
-        |> get("api/repositories")
+        |> get("api/repos")
         |> json_response(200)
       assert length(result) == 3
     end
   end
 
-  describe "GET /api/repositories/:repository" do
+  describe "GET /api/repos/:repository" do
     test "not authorized", %{repository1: repository1, repository2: repository2} do
-      conn = get build_conn(), "api/repositories/#{repository1.name}"
+      conn = get build_conn(), "api/repos/#{repository1.name}"
       result = json_response(conn, 200)
       assert result["name"] == repository1.name
 
-      conn = get build_conn(), "api/repositories/#{repository2.name}"
+      conn = get build_conn(), "api/repos/#{repository2.name}"
       response(conn, 403)
     end
 
@@ -41,7 +41,7 @@ defmodule Hexpm.Web.API.RepositoryControllerTest do
       result =
         build_conn()
         |> put_req_header("authorization", key_for(user))
-        |> get("api/repositories/#{repository2.name}")
+        |> get("api/repos/#{repository2.name}")
         |> json_response(200)
       assert result["name"] == repository2.name
     end
