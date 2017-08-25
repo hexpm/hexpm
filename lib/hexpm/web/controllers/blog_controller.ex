7e79811e4330463be903e82345775a9d7c48431b
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2017-08-25 10:17:49 +0200

    Add blog

---
+++ lib/hexpm/web/controllers/blog_controller.ex
@@ -0,0 +1,32 @@
+defmodule Hexpm.Web.BlogController do
+  use Hexpm.Web, :controller
+
+  def index(conn, _params) do
+    render(conn, "index.html", [
+      title: "Blog",
+      container: "container page blog"
+    ])
+  end
+
+  def show(conn, %{"name" => name}) do
+    if name in all_slugs() do
+      render(conn, "#{name}.html", [
+        title: title(name),
+        container: "container page blog"
+      ])
+    else
+      not_found(conn)
+    end
+  end
+
+  defp all_slugs() do
+    Hexpm.Web.BlogView.all_templates()
+    |> Enum.map(&Path.rootname/1)
+  end
+
+  defp title(slug) do
+    slug
+    |> String.replace("-", " ")
+    |> String.capitalize()
+  end
+end
