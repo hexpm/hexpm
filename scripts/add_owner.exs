0ef0e35558597e6c078163a2c3e1d7b6c907533b
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2017-08-28 22:19:21 +0200

    Add script for adding repositories

--- scripts/add_owner.exs
+++ scripts/add_owner.exs
@@ -1,16 +1,7 @@
-[name, username] = System.argv
+[name, username] = System.argv()
 
 package = Hexpm.Repo.get_by!(Hexpm.Repository.Package, name: name)
-user    = Hexpm.Repo.get_by!(Hexpm.Accounts.User, username: username)
+user = Hexpm.Repo.get_by!(Hexpm.Accounts.User, username: username)
 
-unless package do
-  IO.puts "No package: #{name}"
-  System.halt(1)
-end
-
-unless user do
-  IO.puts "No user: #{username}"
-  System.halt(1)
-end
-
-Hexpm.Repository.Package.build_owner(package, user) |> Hexpm.Repo.insert!
+Hexpm.Repository.Package.build_owner(package, user)
+|> Hexpm.Repo.insert!()
---
+++ scripts/add_repository.exs
@@ -0,0 +1,6 @@
+[username, name] = System.argv
+
+user = Hexpm.Accounts.Users.get(username, [:emails])
+
+Hexpm.Repository.Repositories.create(name, user)
+|> IO.inspect()
