effc736d47d9bd31c2fbc8168ffc12f7718c70af
Wojtek Mach <wojtek@wojtekmach.pl>
2017-09-04 09:29:13 +0200

    Do not show other users private packages on user profile package

--- lib/hexpm/web/controllers/user_controller.ex
+++ lib/hexpm/web/controllers/user_controller.ex
@@ -3,8 +3,11 @@ defmodule Hexpm.Web.UserController do
 
   def show(conn, %{"username" => username}) do
     if user = Users.get_by_username(username, [:emails, owned_packages: :repository]) do
+      repositories = Users.all_repositories(conn.assigns.current_user)
+      repository_ids = Enum.map(repositories, & &1.id)
       packages =
         user.owned_packages
+        |> Enum.filter(& &1.repository_id in repository_ids)
         |> Packages.attach_versions()
         |> Enum.sort_by(&[repository_sort(&1.repository), &1.name])
       public_email = User.email(user, :public)
--- test/hexpm/web/controllers/user_controller_test.exs
+++ test/hexpm/web/controllers/user_controller_test.exs
@@ -2,14 +2,65 @@ defmodule Hexpm.Web.UserControllerTest do
   use Hexpm.ConnCase, async: true
 
   setup do
-    %{user: create_user("eric", "eric@mail.com", "hunter42"), password: "hunter42"}
+    user1 = insert(:user)
+    user2 = insert(:user)
+    user3 = insert(:user)
+
+    repository1 = insert(:repository)
+    repository2 = insert(:repository)
+
+    owners = [build(:package_owner, owner: user1)]
+    package1 = insert(:package, name: "package1", package_owners: owners)
+    package2 = insert(:package, name: "package2", package_owners: owners, repository_id: repository1.id)
+    package3 = insert(:package, name: "package3", repository_id: repository2.id)
+
+    insert(:repository_user, user: user1, repository: repository1)
+    insert(:repository_user, user: user2, repository: repository1)
+
+    %{
+      package1: package1,
+      package2: package2,
+      package3: package3,
+      repository1: repository1,
+      repository2: repository2,
+      user1: user1,
+      user2: user2,
+      user3: user3
+    }
   end
 
   test "show profile page", c do
     conn = build_conn()
-           |> test_login(c.user)
-           |> get("users/#{c.user.username}")
+           |> test_login(c.user1)
+           |> get("users/#{c.user1.username}")
 
-    assert response(conn, 200) =~ c.user.username
+    assert response(conn, 200) =~ c.user1.username
+  end
+
+  test "show owned packages as owner", c do
+    conn = build_conn()
+           |> test_login(c.user1)
+           |> get("users/#{c.user1.username}")
+
+    assert response(conn, 200) =~ c.package1.name
+    assert response(conn, 200) =~ c.package2.name
+  end
+
+  test "show owned packages as user from the same repository", c do
+    conn = build_conn()
+           |> test_login(c.user2)
+           |> get("users/#{c.user1.username}")
+
+    assert response(conn, 200) =~ c.package1.name
+    assert response(conn, 200) =~ c.package2.name
+  end
+
+  test "show owned packages as other user", c do
+    conn = build_conn()
+           |> test_login(c.user3)
+           |> get("users/#{c.user1.username}")
+
+    assert response(conn, 200) =~ c.package1.name
+    refute response(conn, 200) =~ c.package2.name
   end
 end
