5ab2d98fd7e57dc70b2f483a2502c36dc263a74b
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2017-10-22 20:08:58 +0200

    Fix allow_republish script

--- scripts/allow_republish.exs
+++ scripts/allow_republish.exs
@@ -1,4 +1,4 @@
-desctructure [version, name, repo], Enum.reverse(System.argv())
+desctructure([version, name, repo], Enum.reverse(System.argv()))
 
 repository =
   if repo do
@@ -8,23 +8,23 @@ repository =
   end
 
 unless repository do
-  IO.puts "No package: #{repo}"
+  IO.puts("No package: #{repo}")
   System.halt(1)
 end
 
 package = Hexpm.Repo.get_by!(Hexpm.Repository.Package, name: name, repository_id: repository.id)
 
 unless package do
-  IO.puts "No package: #{name}"
+  IO.puts("No package: #{name}")
   System.halt(1)
 end
 
-release = Hexpm.Repo.get_by!(assoc(package, :releases), version: version)
+release = Hexpm.Repo.get_by!(Ecto.assoc(package, :releases), version: version)
 
 unless release do
-  IO.puts "No release: #{name} #{version}"
+  IO.puts("No release: #{name} #{version}")
   System.halt(1)
 end
 
-Ecto.Changeset.change(release, release | inserted_at: NaiveDateTime.utc_now)
-|> Hexpm.Repo.update!(release)
+Ecto.Changeset.change(release, %{inserted_at: NaiveDateTime.utc_now()})
+|> Hexpm.Repo.update!()
