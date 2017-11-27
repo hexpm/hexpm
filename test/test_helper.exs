d91cf3afb98203e14bb651d90216d393917704a1
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2017-11-27 12:18:22 +0100

    Delete tmp directory before testing

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,5 +1,9 @@
 ExUnit.start()
 
+tmp_dir = Application.get_env(:hexpm, :tmp_dir)
+File.rm_rf(tmp_dir)
+File.mkdir_p(tmp_dir)
+
 Hexpm.BlockAddress.reload()
 Ecto.Adapters.SQL.Sandbox.mode(Hexpm.Repo, :manual)
 Hexpm.Fake.start()
