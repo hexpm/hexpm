51142acaf8a14a1bb0d6d124fb03a3dadd8f2064
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2017-11-27 16:24:39 +0100

    Handle AWS SES throttling

--- lib/hexpm/emails/bamboo.ex
+++ lib/hexpm/emails/bamboo.ex
@@ -1,5 +1,9 @@
 defmodule Hexpm.Emails.Bamboo.SESAdapter do
+  require Logger
+
   @behaviour Bamboo.Adapter
+  @backoff 100
+  @backoff_times 5
 
   def deliver(email, _config) do
     if email.headers != %{} do
@@ -14,12 +18,36 @@ defmodule Hexpm.Emails.Bamboo.SESAdapter do
 
     message = ExAws.SES.build_message(email.html_body, email.text_body, email.subject)
 
-    ExAws.SES.send_email(destination, message, email(email.from), [])
-    |> ExAws.request!()
+    request = ExAws.SES.send_email(destination, message, email(email.from), [])
+    send_email(request, email, 0)
   end
 
   def handle_config(config) do
     config
+  end
+
+  defp send_email(request, email, times)  do
+    request
+    |> ExAws.request()
+    |> maybe_retry(request, email, times)
+  end
+
+  defp maybe_retry({:error, {:http_error, 454, _body}} = error, request, email, times) do
+    if times > @backoff_times do
+      Logger.warn("AWS SES throttled ##{times}")
+      raise "failed to send email\n\n#{inspect email}\n\n#{inspect error}"
+    else
+      Process.sleep(@backoff * trunc(:math.pow(2, times)))
+      send_email(request, email, times + 1)
+    end
+  end
+
+  defp maybe_retry({:error, _} = error, _request, email, _times) do
+    raise "failed to send email\n\n#{inspect email}\n\n#{inspect error}"
+  end
+
+  defp maybe_retry({:ok, result}, _request, _email, _times) do
+    result
   end
 
   defp emails(emails), do: emails |> List.wrap() |> Enum.map(&email/1)
