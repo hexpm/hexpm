a52e4d8baedd89fb777dfe70699c6632a20d8699
supersimple <toddresudek@gmail.com>
2018-01-12 13:18:45 -0700

    Adds retirement information to the package api

--- lib/hexpm/web/views/api/retirement_view.ex
+++ lib/hexpm/web/views/api/retirement_view.ex
@@ -4,11 +4,21 @@ defmodule Hexpm.Web.API.RetirementView do
   def render("show." <> _, %{retirement: retirement}) do
     render_one(retirement, __MODULE__, "show")
   end
+  def render("minimal." <> _, %{retirement: retirement}) do
+    render_one(retirement, __MODULE__, "minimal")
+  end
 
   def render("show", %{retirement: retirement}) do
     %{
       message: retirement.message,
       reason: retirement.reason,
+    }
+  end
+
+  def render("minimal", %{retirement: %{retirement: retirement}}) when is_nil(retirement), do: %{}
+  def render("minimal", %{retirement: %{retirement: retirement, version: version}}) do
+    %{
+      version => %{reason: retirement.reason, message: retirement.message}
     }
   end
 end
