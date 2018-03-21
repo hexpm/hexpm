20fb8266861d973aecec776439adfdcb50330aa5
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2018-03-21 23:29:22 +0100

    Increase timeout on checkout request

--- lib/hexpm/billing/hexpm.ex
+++ lib/hexpm/billing/hexpm.ex
@@ -2,33 +2,33 @@ defmodule Hexpm.Billing.Hexpm do
   @behaviour Hexpm.Billing
 
   def checkout(repository, data) do
-    case post("api/customers/#{repository}/payment_source", data) do
+    case post("api/customers/#{repository}/payment_source", data, [recv_timeout: 15_000]) do
       {:ok, 204, _headers, body} -> {:ok, body}
       {:ok, 422, _headers, body} -> {:error, body}
     end
   end
 
   def dashboard(repository) do
-    case get_json("api/customers/#{repository}") do
+    case get_json("api/customers/#{repository}", []) do
       {:ok, 200, _headers, body} -> body
       {:ok, 404, _headers, _body} -> nil
     end
   end
 
   def cancel(repository) do
-    {:ok, 200, _headers, body} = post("api/customers/#{repository}/cancel", %{})
+    {:ok, 200, _headers, body} = post("api/customers/#{repository}/cancel", %{}, [])
     body
   end
 
   def create(params) do
-    case post("api/customers", params) do
+    case post("api/customers", params, []) do
       {:ok, 200, _headers, body} -> {:ok, body}
       {:ok, 422, _headers, body} -> {:error, body}
     end
   end
 
   def update(repository, params) do
-    case patch("api/customers/#{repository}", params) do
+    case patch("api/customers/#{repository}", params, []) do
       {:ok, 200, _headers, body} -> {:ok, body}
       {:ok, 404, _headers, _body} -> {:ok, nil}
       {:ok, 422, _headers, body} -> {:error, body}
@@ -36,12 +36,12 @@ defmodule Hexpm.Billing.Hexpm do
   end
 
   def invoice(id) do
-    {:ok, 200, _headers, body} = get_html("api/invoices/html/#{id}")
+    {:ok, 200, _headers, body} = get_html("api/invoices/html/#{id}", [])
     body
   end
 
   def report() do
-    {:ok, 200, _headers, body} = get_json("api/reports/customers")
+    {:ok, 200, _headers, body} = get_json("api/reports/customers", [])
     body
   end
 
@@ -49,7 +49,7 @@ defmodule Hexpm.Billing.Hexpm do
     Application.get_env(:hexpm, :billing_key)
   end
 
-  defp post(url, body) do
+  defp post(url, body, opts) do
     url = Application.get_env(:hexpm, :billing_url) <> url
 
     headers = [
@@ -60,11 +60,11 @@ defmodule Hexpm.Billing.Hexpm do
 
     body = Hexpm.Web.Jiffy.encode!(body)
 
-    :hackney.post(url, headers, body, [])
+    :hackney.post(url, headers, body, opts)
     |> read_request()
   end
 
-  defp patch(url, body) do
+  defp patch(url, body, opts) do
     url = Application.get_env(:hexpm, :billing_url) <> url
 
     headers = [
@@ -75,11 +75,11 @@ defmodule Hexpm.Billing.Hexpm do
 
     body = Hexpm.Web.Jiffy.encode!(body)
 
-    :hackney.patch(url, headers, body, [])
+    :hackney.patch(url, headers, body, opts)
     |> read_request()
   end
 
-  defp get_json(url) do
+  defp get_json(url, opts) do
     url = Application.get_env(:hexpm, :billing_url) <> url
 
     headers = [
@@ -87,11 +87,11 @@ defmodule Hexpm.Billing.Hexpm do
       {"accept", "application/json"}
     ]
 
-    :hackney.get(url, headers, [])
+    :hackney.get(url, headers, opts)
     |> read_request()
   end
 
-  defp get_html(url) do
+  defp get_html(url, opts) do
     url = Application.get_env(:hexpm, :billing_url) <> url
 
     headers = [
@@ -99,7 +99,7 @@ defmodule Hexpm.Billing.Hexpm do
       {"accept", "text/html"}
     ]
 
-    :hackney.get(url, headers, [])
+    :hackney.get(url, headers, opts)
     |> read_request()
   end
 
