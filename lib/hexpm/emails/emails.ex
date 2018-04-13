07dc1f3d7cf0e79d60dadd9332bcae15de9c5a5d
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2018-04-13 08:45:09 +0200

    Remove billing emails

--- lib/hexpm/emails/emails.ex
+++ lib/hexpm/emails/emails.ex
@@ -65,22 +65,6 @@ defmodule Hexpm.Emails do
     |> render("repository_invite.html")
   end
 
-  def organizations_live(user) do
-    email()
-    |> to(user)
-    |> subject("Hex.pm - Private packages and organizations going live")
-    |> assign(:username, user.username)
-    |> render("organizations_live.html")
-  end
-
-  def billing_reminder(user) do
-    email()
-    |> to(user)
-    |> subject("Hex.pm - Organization free beta period is ending")
-    |> assign(:username, user.username)
-    |> render("billing_reminder.html")
-  end
-
   defp email() do
     new_email()
     |> from(source())
