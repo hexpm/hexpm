37ada7e5e802dceceb914e76799ae237e56caae7
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2018-05-29 18:34:05 +0200

    Update references in keys when renaming repository

--- scripts/rename_repository.exs
+++ scripts/rename_repository.exs
@@ -12,9 +12,38 @@ IO.inspect(repository)
 answer = IO.gets("Rename? [Yn] ")
 
 if answer =~ ~r/^(Y(es)?)?$/i do
-  repository
-  |> Ecto.Changeset.change(name: new_name)
-  |> Hexpm.Repo.update!()
+  Hexpm.Repo.transaction(fn ->
+    repository
+    |> Ecto.Changeset.change(name: new_name)
+    |> Hexpm.Repo.update!()
+
+    keys = Repo.all(Key)
+
+    Enum.each(keys, fn key ->
+      yes? =
+        Enum.any?(key.permissions, fn permission ->
+          permission.domain == "repository" and permission.resource == old_name
+        end)
+
+      if yes? do
+        permissions =
+          Enum.map(key.permissions, fn permission ->
+            if permission.domain == "repository"  do
+              Ecto.Changeset.change(permission, resource: new_name)
+            else
+              permission
+            end
+          end)
+
+        key
+        |> Ecto.Changeset.change()
+        |> Ecto.Changeset.put_embed(:permissions, permissions)
+        |> Repo.update!()
+
+        IO.puts "#{key.name} - #{key.id}"
+      end
+    end)
+  end)
 
   IO.puts("Renamed")
 else
