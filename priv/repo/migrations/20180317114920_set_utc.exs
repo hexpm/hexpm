d5e29eff96e76ca0554514df94cb8a185edbed84
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2018-09-25 00:08:47 +0200

    Fix migration for RepoBase

--- priv/repo/migrations/20180317114920_set_utc.exs
+++ priv/repo/migrations/20180317114920_set_utc.exs
@@ -2,7 +2,7 @@ defmodule Hexpm.Repo.Migrations.SetUtc do
   use Ecto.Migration
 
   def change do
-    database_name = Keyword.fetch!(Hexpm.Repo.config(), :database)
+    database_name = Keyword.fetch!(Hexpm.RepoBase.config(), :database)
     execute("ALTER DATABASE #{database_name} SET timezone TO 'UTC'")
   end
 end
