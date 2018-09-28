9872a27e8b3819308f72732062f89c1b669e9476
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2018-09-28 19:21:04 +0200

    Fix blog when running as release

--- lib/hexpm/web/views/blog_view.ex
+++ lib/hexpm/web/views/blog_view.ex
@@ -1,7 +1,21 @@
 defmodule Hexpm.Web.BlogView do
   use Hexpm.Web, :view
 
-  @skip_slugs ~w()
+  skip_slugs = ~w()
+
+  all_templates =
+    Phoenix.Template.find_all(@phoenix_root)
+    |> Enum.map(&Phoenix.Template.template_path_to_name(&1, @phoenix_root))
+    |> Enum.flat_map(fn
+      <<n1, n2, n3, "-", slug::binary>> = template
+      when n1 in ?0..?9 and n2 in ?0..?9 and n3 in ?0..?9 ->
+        [{Path.rootname(slug), template}]
+
+      _other ->
+        []
+    end)
+    |> Enum.reject(fn {slug, _template} -> slug in skip_slugs end)
+    |> Enum.sort_by(&elem(&1, 1), &>=/2)
 
   def render("index.html", _assigns) do
     posts =
@@ -25,18 +39,7 @@ defmodule Hexpm.Web.BlogView do
   end
 
   def all_templates() do
-    Phoenix.Template.find_all(@phoenix_root)
-    |> Enum.map(&Phoenix.Template.template_path_to_name(&1, @phoenix_root))
-    |> Enum.flat_map(fn
-      <<n1, n2, n3, "-", slug::binary>> = template
-      when n1 in ?0..?9 and n2 in ?0..?9 and n3 in ?0..?9 ->
-        [{Path.rootname(slug), template}]
-
-      _other ->
-        []
-    end)
-    |> Enum.reject(fn {slug, _template} -> slug in @skip_slugs end)
-    |> Enum.sort_by(&elem(&1, 1), &>=/2)
+    unquote(all_templates)
   end
 
   defp first_paragraph(content) do
