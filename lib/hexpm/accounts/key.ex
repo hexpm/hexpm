21eb0bf8a0c40ae1ccfd5cd2c79eba7d9359fabe
Wojtek Mach <wojtek@wojtekmach.pl>
2018-10-10 20:28:11 +0200

    Remove unnecessary fragment

--- lib/hexpm/accounts/key.ex
+++ lib/hexpm/accounts/key.ex
@@ -107,7 +107,7 @@ defmodule Hexpm.Accounts.Key do
       where: k.name == ^key_name and not query_revoked(k),
       update: [
         set: [
-          revoked_at: fragment("?", ^revoked_at),
+          revoked_at: ^revoked_at,
           updated_at: ^DateTime.utc_now()
         ]
       ]
@@ -120,7 +120,7 @@ defmodule Hexpm.Accounts.Key do
       where: not query_revoked(k),
       update: [
         set: [
-          revoked_at: fragment("?", ^revoked_at),
+          revoked_at: ^revoked_at,
           updated_at: ^DateTime.utc_now()
         ]
       ]
