9711c6896f82b3bd608b444be7944877076d8ea6
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2019-04-19 21:07:38 +0200

    Reserve package

--- lib/hexpm/repository/package.ex
+++ lib/hexpm/repository/package.ex
@@ -31,7 +31,7 @@ defmodule Hexpm.Repository.Package do
     webtool wx xmerl
   )
   @inets_names ~w(ftp tftp httpc httpd http_uri)
-  @app_names ~w(firenest toucan net http net_http gen_http xhttp)
+  @app_names ~w(firenest toucan phoenix_live_view net http net_http)
   @windows_names ~w(
     nul con prn aux com1 com2 com3 com4 com5 com6 com7 com8 com9 lpt1 lpt2
     lpt3 lpt4 lpt5 lpt6 lpt7 lpt8 lpt9
