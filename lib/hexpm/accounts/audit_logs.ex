d78b0216039ecad288b8d126ab4496bdc27fc4d5
Yiming Chen <dsdshcym@gmail.com>
2019-06-05 20:07:42 +0800

    Add Hexpm.Accounts.AuditLogs.all_by/1 returns logs created by a user

--- lib/hexpm/accounts/audit_logs.ex
+++ lib/hexpm/accounts/audit_logs.ex
@@ -1,3 +1,10 @@
 defmodule Hexpm.Accounts.AuditLogs do
   use HexpmWeb, :context
+
+  alias Hexpm.Accounts.AuditLog
+
+  def all_by(user) do
+    from(l in AuditLog, where: l.user_id == ^user.id)
+    |> Repo.all()
+  end
 end
--- test/hexpm/accounts/audit_logs_test.exs
+++ test/hexpm/accounts/audit_logs_test.exs
@@ -1,3 +1,23 @@
 defmodule Hexpm.Accounts.AuditLogsTest do
   use Hexpm.DataCase, async: true
+
+  alias Hexpm.Accounts.AuditLogs
+
+  describe "all_by(user)" do
+    test "returns audit_logs belong to this user" do
+      this_user = insert(:user)
+      audit_log = insert(:audit_log, user: this_user)
+
+      assert [audit_log_fetched] = AuditLogs.all_by(this_user)
+      assert audit_log_fetched.id == audit_log.id
+    end
+
+    test "does not return audit_logs that do not belong to this user" do
+      this_user = insert(:user)
+      other_user = insert(:user)
+      _audit_log = insert(:audit_log, user: other_user)
+
+      assert [] = AuditLogs.all_by(this_user)
+    end
+  end
 end
