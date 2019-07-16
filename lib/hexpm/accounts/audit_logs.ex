28f342cdb2c98e5b2cf9117b17a891353b20fe1b
Yiming Chen <dsdshcym@gmail.com>
2019-07-16 19:59:52 +0800

    Remove default per_page value for AuditLogs.all_by
    - we are going to load different amounts of audit_logs in different places
      1. 10 (maybe) for package/user/organization show pages
      2. 100 for api endpoints
    - but neither 100 nor 10 is a reasonable choice here

--- lib/hexpm/accounts/audit_logs.ex
+++ lib/hexpm/accounts/audit_logs.ex
@@ -3,14 +3,12 @@ defmodule Hexpm.Accounts.AuditLogs do
 
   alias Hexpm.Accounts.AuditLog
 
-  @default_per_page 100
-
   def all_by(schema) do
     AuditLog.all_by(schema)
     |> Repo.all()
   end
 
-  def all_by(schema, page, per_page \\ @default_per_page) do
+  def all_by(schema, page, per_page) do
     AuditLog.all_by(schema)
     |> Hexpm.Utils.paginate(page, per_page)
     |> Repo.all()
