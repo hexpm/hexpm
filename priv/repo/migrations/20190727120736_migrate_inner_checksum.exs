5da78079a371e99d1497f3ddc3c05a3e781818bb
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2019-07-25 13:45:19 +0200

    Add outer checksum field

--- priv/repo/migrations/20190727120736_migrate_inner_checksum.exs
+++ priv/repo/migrations/20190727120736_migrate_inner_checksum.exs
@@ -2,22 +2,24 @@ defmodule Hexpm.RepoBase.Migrations.MigrateInnerChecksum do
   use Ecto.Migration
 
   def up do
-    execute """
+    execute("""
     UPDATE releases SET inner_checksum = decode(checksum, 'hex')
-    """
+    """)
 
     alter table(:releases) do
+      modify(:checksum, :string, null: true)
       modify(:inner_checksum, :binary, null: false)
     end
   end
 
   def down do
     alter table(:releases) do
+      modify(:checksum, :string, null: false)
       modify(:inner_checksum, :binary, null: true)
     end
 
-    execute """
+    execute("""
     UPDATE releases SET inner_checksum = NULL
-    """
+    """)
   end
 end
