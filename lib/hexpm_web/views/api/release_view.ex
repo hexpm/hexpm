5e4a047cba72b40406bfa57d6dc16251e4a45b66
Jan Krutisch <jan@krutisch.de>
2019-09-24 11:15:25 +0200

    WIP: Add dates to minimal releases view

--- lib/hexpm_web/views/api/release_view.ex
+++ lib/hexpm_web/views/api/release_view.ex
@@ -37,7 +37,9 @@ defmodule HexpmWeb.API.ReleaseView do
     %{
       version: release.version,
       url: url_for_release(package, release),
-      has_docs: release.has_docs
+      has_docs: release.has_docs,
+      inserted_at: release.inserted_at,
+      updated_at: release.updated_at
     }
   end
 
