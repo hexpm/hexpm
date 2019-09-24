6ce601852833207d91292272bf68fa5a7e7b557f
Yiming Chen <dsdshcym@users.noreply.github.com>
2019-09-25 02:00:28 +0800

    Order audit logs by newest first (#858)

--- lib/hexpm/accounts/audit_logs.ex
+++ lib/hexpm/accounts/audit_logs.ex
@@ -5,11 +5,13 @@ defmodule Hexpm.Accounts.AuditLogs do
 
   def all_by(schema) do
     AuditLog.all_by(schema)
+    |> AuditLog.newest_first()
     |> Repo.all()
   end
 
   def all_by(schema, page, per_page) do
     AuditLog.all_by(schema)
+    |> AuditLog.newest_first()
     |> Hexpm.Utils.paginate(page, per_page)
     |> Repo.all()
   end
