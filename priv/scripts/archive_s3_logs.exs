04dc707206b4d8b79e9b6a23f43dc5155c51e6f9
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2020-01-03 21:12:58 +0100

    Delete file after uploading

--- priv/scripts/archive_s3_logs.exs
+++ priv/scripts/archive_s3_logs.exs
@@ -69,6 +69,8 @@ if action == "upload" || action == "fetch-and-upload" do
   ExAws.S3.Upload.stream_file(filepath, [:read_ahead])
   |> ExAws.S3.upload("backup.hex.pm", key, timeout: 600_000)
   |> ExAws.request!(region: "us-east-1")
+
+  File.rm!(filepath)
 end
 
 if action == "delete" do
