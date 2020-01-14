9a832b806ab369afbccb1f176f92ce89146be9fd
Yiming Chen <dsdshcym@gmail.com>
2020-01-14 20:25:18 +0800

    Add AuditLogView.humanize_audit_log_info/1
    - translate an audit_log to human readable descriptions

--- lib/hexpm_web/views/dashboard/audit_log_view.ex
+++ lib/hexpm_web/views/dashboard/audit_log_view.ex
@@ -2,4 +2,99 @@ defmodule HexpmWeb.Dashboard.AuditLogView do
   use HexpmWeb, :view
 
   alias HexpmWeb.DashboardView
+
+  @doc """
+  Translate an audit_log to user readable descriptions
+  """
+  def humanize_audit_log_info(log = %{action: "docs.publish"}) do
+    "Publish documentation for #{log.params["package"]["name"]} (#{
+      log.params["release"]["version"]
+    })"
+  end
+
+  def humanize_audit_log_info(log = %{action: "docs.revert"}) do
+    "Revert documentation for #{log.params["package"]["name"]} (#{
+      log.params["release"]["version"]
+    })"
+  end
+
+  def humanize_audit_log_info(log = %{action: "key.generate"}) do
+    "Generate key #{log.params["name"]}"
+  end
+
+  def humanize_audit_log_info(log = %{action: "key.remove"}) do
+    "Remove key #{log.params["name"]}"
+  end
+
+  def humanize_audit_log_info(log = %{action: "owner.add"}) do
+    "Add #{log.params["user"]["username"]} as a new owner of package #{
+      log.params["package"]["name"]
+    }"
+  end
+
+  def humanize_audit_log_info(log = %{action: "owner.transfer"}) do
+    "Transfer package #{log.params["package"]["name"]} to #{log.params["user"]["username"]}"
+  end
+
+  def humanize_audit_log_info(log = %{action: "owner.remove"}) do
+    "Remove #{log.params["user"]["username"]} from owners of package #{
+      log.params["package"]["name"]
+    }"
+  end
+
+  def humanize_audit_log_info(log = %{action: "release.publish"}) do
+    "Publish package #{log.params["package"]["name"]} version #{log.params["release"]["version"]}"
+  end
+
+  def humanize_audit_log_info(log = %{action: "release.revert"}) do
+    "Revert package #{log.params["package"]["name"]} version #{log.params["release"]["version"]}"
+  end
+
+  def humanize_audit_log_info(log = %{action: "release.retire"}) do
+    "Retire package #{log.params["package"]["name"]} version #{log.params["release"]["version"]}"
+  end
+
+  def humanize_audit_log_info(log = %{action: "release.unretire"}) do
+    "Unretire package #{log.params["package"]["name"]} version #{log.params["release"]["version"]}"
+  end
+
+  def humanize_audit_log_info(log = %{action: "email.add"}) do
+    "Add email #{log.params["email"]}"
+  end
+
+  def humanize_audit_log_info(log = %{action: "email.remove"}) do
+    "Remove email #{log.params["email"]}"
+  end
+
+  def humanize_audit_log_info(log = %{action: "email.primary"}) do
+    "Set email #{log.params["email"]} as primary email"
+  end
+
+  def humanize_audit_log_info(log = %{action: "email.public"}) do
+    "Set email #{log.params["email"]} as public email"
+  end
+
+  def humanize_audit_log_info(log = %{action: "email.gravatar"}) do
+    "Set email #{log.params["email"]} as gravatar email"
+  end
+
+  def humanize_audit_log_info(_log = %{action: "user.create"}) do
+    "Create user account"
+  end
+
+  def humanize_audit_log_info(_log = %{action: "user.update"}) do
+    "Update user profile"
+  end
+
+  def humanize_audit_log_info(_log = %{action: "password.reset.init"}) do
+    "Request to reset password"
+  end
+
+  def humanize_audit_log_info(_log = %{action: "password.reset.finish"}) do
+    "Reset password successfully"
+  end
+
+  def humanize_audit_log_info(_log = %{action: "password.update"}) do
+    "Update password"
+  end
 end
---
+++ test/hexpm_web/views/dashboard/audit_log_view_test.exs
@@ -0,0 +1,176 @@
+defmodule HexpmWeb.Dashboard.AuditLogViewTest do
+  use HexpmWeb.ConnCase, async: true
+
+  alias HexpmWeb.Dashboard.AuditLogView
+
+  describe "humanize_audit_log_info/1" do
+    test "doc.publish" do
+      log =
+        build(:audit_log,
+          action: "docs.publish",
+          params: %{"package" => %{"name" => "Awesome"}, "release" => %{"version" => "0.2.4"}}
+        )
+
+      assert AuditLogView.humanize_audit_log_info(log) ==
+               "Publish documentation for Awesome (0.2.4)"
+    end
+
+    test "docs.revert" do
+      log =
+        build(:audit_log,
+          action: "docs.revert",
+          params: %{"package" => %{"name" => "Awesome"}, "release" => %{"version" => "0.2.4"}}
+        )
+
+      assert AuditLogView.humanize_audit_log_info(log) ==
+               "Revert documentation for Awesome (0.2.4)"
+    end
+
+    test "key.generate" do
+      log = build(:audit_log, action: "key.generate", params: %{"name" => "Secret"})
+
+      assert AuditLogView.humanize_audit_log_info(log) == "Generate key Secret"
+    end
+
+    test "key.remove" do
+      log = build(:audit_log, action: "key.remove", params: %{"name" => "Secret"})
+
+      assert AuditLogView.humanize_audit_log_info(log) == "Remove key Secret"
+    end
+
+    test "owner.add" do
+      log =
+        build(:audit_log,
+          action: "owner.add",
+          params: %{"package" => %{"name" => "Awesome"}, "user" => %{"username" => "John"}}
+        )
+
+      assert AuditLogView.humanize_audit_log_info(log) ==
+               "Add John as a new owner of package Awesome"
+    end
+
+    test "owner.transfer" do
+      log =
+        build(:audit_log,
+          action: "owner.transfer",
+          params: %{"package" => %{"name" => "Awesome"}, "user" => %{"username" => "John"}}
+        )
+
+      assert AuditLogView.humanize_audit_log_info(log) == "Transfer package Awesome to John"
+    end
+
+    test "owner.remove" do
+      log =
+        build(:audit_log,
+          action: "owner.remove",
+          params: %{"package" => %{"name" => "Awesome"}, "user" => %{"username" => "John"}}
+        )
+
+      assert AuditLogView.humanize_audit_log_info(log) ==
+               "Remove John from owners of package Awesome"
+    end
+
+    test "release.publish" do
+      log =
+        build(:audit_log,
+          action: "release.publish",
+          params: %{"package" => %{"name" => "Awesome"}, "release" => %{"version" => "1.0.0"}}
+        )
+
+      assert AuditLogView.humanize_audit_log_info(log) == "Publish package Awesome version 1.0.0"
+    end
+
+    test "release.revert" do
+      log =
+        build(:audit_log,
+          action: "release.revert",
+          params: %{"package" => %{"name" => "Awesome"}, "release" => %{"version" => "1.0.0"}}
+        )
+
+      assert AuditLogView.humanize_audit_log_info(log) == "Revert package Awesome version 1.0.0"
+    end
+
+    test "release.retire" do
+      log =
+        build(:audit_log,
+          action: "release.retire",
+          params: %{"package" => %{"name" => "Awesome"}, "release" => %{"version" => "1.0.0"}}
+        )
+
+      assert AuditLogView.humanize_audit_log_info(log) == "Retire package Awesome version 1.0.0"
+    end
+
+    test "release.unretire" do
+      log =
+        build(:audit_log,
+          action: "release.unretire",
+          params: %{"package" => %{"name" => "Awesome"}, "release" => %{"version" => "1.0.0"}}
+        )
+
+      assert AuditLogView.humanize_audit_log_info(log) == "Unretire package Awesome version 1.0.0"
+    end
+
+    test "email.add" do
+      log = build(:audit_log, action: "email.add", params: %{"email" => "test@example.com"})
+
+      assert AuditLogView.humanize_audit_log_info(log) == "Add email test@example.com"
+    end
+
+    test "email.remove" do
+      log = build(:audit_log, action: "email.remove", params: %{"email" => "test@example.com"})
+
+      assert AuditLogView.humanize_audit_log_info(log) == "Remove email test@example.com"
+    end
+
+    test "email.primary" do
+      log = build(:audit_log, action: "email.primary", params: %{"email" => "test@example.com"})
+
+      assert AuditLogView.humanize_audit_log_info(log) ==
+               "Set email test@example.com as primary email"
+    end
+
+    test "email.public" do
+      log = build(:audit_log, action: "email.public", params: %{"email" => "test@example.com"})
+
+      assert AuditLogView.humanize_audit_log_info(log) ==
+               "Set email test@example.com as public email"
+    end
+
+    test "email.gravatar" do
+      log = build(:audit_log, action: "email.gravatar", params: %{"email" => "test@example.com"})
+
+      assert AuditLogView.humanize_audit_log_info(log) ==
+               "Set email test@example.com as gravatar email"
+    end
+
+    test "user.create" do
+      log = build(:audit_log, action: "user.create")
+
+      assert AuditLogView.humanize_audit_log_info(log) == "Create user account"
+    end
+
+    test "user.update" do
+      log = build(:audit_log, action: "user.update")
+
+      assert AuditLogView.humanize_audit_log_info(log) == "Update user profile"
+    end
+
+    test "password.reset.init" do
+      log = build(:audit_log, action: "password.reset.init")
+
+      assert AuditLogView.humanize_audit_log_info(log) == "Request to reset password"
+    end
+
+    test "password.reset.finish" do
+      log = build(:audit_log, action: "password.reset.finish")
+
+      assert AuditLogView.humanize_audit_log_info(log) == "Reset password successfully"
+    end
+
+    test "password.update" do
+      log = build(:audit_log, action: "password.update")
+
+      assert AuditLogView.humanize_audit_log_info(log) == "Update password"
+    end
+  end
+end
