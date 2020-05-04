9b676bef56ad5cd5c2def73489476890430fcc82
Todd Resudek <toddresudek@gmail.com>
2020-05-04 08:16:51 -0600

    change API to return entire short url instead of short code (#931)
    * change API to return entire short url instead of short code
    
    * disambiguate the short_url params
    
    * formatting
    
    * Update test/hexpm_web/controllers/api/short_url_controller_test.exs
    
    Co-authored-by: Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
    
    Co-authored-by: Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>

--- lib/hexpm_web/views/api/short_url_view.ex
+++ lib/hexpm_web/views/api/short_url_view.ex
@@ -1,11 +1,11 @@
 defmodule HexpmWeb.API.ShortURLView do
   use HexpmWeb, :view
 
-  def render("show." <> _, %{short_url: short_url}) do
-    render(__MODULE__, "show", short_url: short_url)
+  def render("show." <> _, %{url: url}) do
+    render(__MODULE__, "show", url: url)
   end
 
-  def render("show", %{short_url: short_url}) do
-    %{short_code: short_url.short_code}
+  def render("show", %{url: url}) do
+    %{url: url}
   end
 end
--- test/hexpm_web/controllers/api/short_url_controller_test.exs
+++ test/hexpm_web/controllers/api/short_url_controller_test.exs
@@ -3,9 +3,12 @@ defmodule HexpmWeb.API.ShortURLControllerTest do
 
   describe "post /api/short_url" do
     test "creates a short_code" do
-      assert build_conn()
-             |> post("api/short_url", %{"url" => "https://diff.hex.pm?diff[]=ecto:3.0.0:3.0.1"})
-             |> response(201)
+      assert %{"url" => url} =
+               build_conn()
+               |> post("api/short_url", %{"url" => "https://diff.hex.pm?diff[]=ecto:3.0.0:3.0.1"})
+               |> json_response(201)
+
+      assert url =~ ~r/\/l\/[\w\d]{5}/
     end
 
     test "fails given an invalid url" do
