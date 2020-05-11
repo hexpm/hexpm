eef458680c773cb422efef291ac77a4add166d44
Todd Resudek <toddresudek@gmail.com>
2020-05-11 09:19:34 -0600

    rebase cleanup

--- lib/hexpm_web/endpoint.ex
+++ lib/hexpm_web/endpoint.ex
@@ -49,7 +49,6 @@ defmodule HexpmWeb.Endpoint do
   plug HexpmWeb.Router
 
   def init(_key, config) do
-IO.inspect(config)
     if config[:load_from_system_env] do
       port = System.get_env("HEXPM_PORT")
 
