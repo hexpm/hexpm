9173fd60ba7ccd5a443e74355812a91bc59c6c74
Todd Resudek <toddresudek@gmail.com>
2020-08-12 22:15:37 -0600

    Adds data to the user packages (#952)
    * Adds data to the user packages

--- lib/hexpm_web/controllers/user_controller.ex
+++ lib/hexpm_web/controllers/user_controller.ex
@@ -2,7 +2,12 @@ defmodule HexpmWeb.UserController do
   use HexpmWeb, :controller
 
   def show(conn, %{"username" => username}) do
-    user = Users.get_by_username(username, [:emails, :organization, owned_packages: :repository])
+    user =
+      Users.get_by_username(username, [
+        :emails,
+        :organization,
+        owned_packages: [:repository, :downloads]
+      ])
 
     if user do
       organization = user.organization
@@ -27,6 +32,11 @@ defmodule HexpmWeb.UserController do
       Packages.accessible_user_owned_packages(user, conn.assigns.current_user)
       |> Packages.attach_versions()
 
+    downloads = Packages.packages_downloads_with_all_views(packages)
+
+    total_downloads =
+      Enum.reduce(downloads, 0, fn {_id, d}, acc -> acc + Map.get(d, "all", 0) end)
+
     public_email = User.email(user, :public)
     gravatar_email = User.email(user, :gravatar)
 
@@ -37,6 +47,8 @@ defmodule HexpmWeb.UserController do
       container: "container page user",
       user: user,
       packages: packages,
+      downloads: downloads,
+      total_downloads: total_downloads,
       public_email: public_email,
       gravatar_email: gravatar_email
     )
