b1b9939df0fcca9171bb0cb94c86b0667bcc2458
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2020-11-23 18:39:15 +0100

    Update to Tailwind CSS 2.0

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,10 +1,6 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
   purge: {
     enabled: process.env.NODE_ENV === 'production',
     content: [
@@ -18,10 +14,8 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
-    },
+    }
   },
   variants: {},
-  plugins: [
-    require('@tailwindcss/ui')
-  ],
+  plugins: []
 }
