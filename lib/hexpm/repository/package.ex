be55cebbf714632ff282867fb6f9957a66282727
José Valim <jose.valim@gmail.com>
2021-02-25 16:30:11 +0100

    Update package.ex

--- lib/hexpm/repository/package.ex
+++ lib/hexpm/repository/package.ex
@@ -32,7 +32,7 @@ defmodule Hexpm.Repository.Package do
     webtool wx xmerl
   )
   @inets_names ~w(ftp tftp httpc httpd)
-  @app_names ~w(toucan exla nx live_book axon net http net_http mint_pool)
+  @app_names ~w(toucan exla nx live_book axon torchx net http net_http mint_pool)
   @windows_names ~w(
     nul con prn aux com1 com2 com3 com4 com5 com6 com7 com8 com9 lpt1 lpt2
     lpt3 lpt4 lpt5 lpt6 lpt7 lpt8 lpt9
