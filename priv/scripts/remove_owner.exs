06747bba2dd01897179781a4e8284e1f3f7adf9b
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2021-07-08 19:57:38 +0200

    Fix ownership scripts

--- priv/scripts/remove_owner.exs
+++ priv/scripts/remove_owner.exs
@@ -1,6 +1,6 @@
 destructure [package, username_or_email], System.argv()
 
-package = Packages.get(package)
+package = Packages.get("hexpm", package)
 user = Users.get(username_or_email, [:emails])
 
 IO.inspect(Owners.remove(package, user, audit: {Users.get("admin"), "CLI"}))
