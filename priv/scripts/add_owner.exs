e023956dda1e3d36ddd4d7700bbf6d093614853e
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2021-07-08 20:05:09 +0200

    Format add_owner script

--- priv/scripts/add_owner.exs
+++ priv/scripts/add_owner.exs
@@ -3,7 +3,7 @@ destructure [package, username_or_email, level], System.argv()
 alias Hexpm.Repository.{Owners, Packages}
 alias Hexpm.Accounts.Users
 
-package = Hexpm.Repo.preload(Packages.get("hexpm", package), [repository: :organization])
+package = Hexpm.Repo.preload(Packages.get("hexpm", package), repository: :organization)
 user = Users.get(username_or_email, [:emails])
 params = if level, do: %{"level" => level}, else: %{}
 
