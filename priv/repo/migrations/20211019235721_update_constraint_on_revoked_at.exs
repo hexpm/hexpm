1d510364aa7af52985ce669f7ea8c58b5789cf80
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2021-10-27 12:01:58 +0200

    Add keys_organization_id_name_revoked_at_key

--- priv/repo/migrations/20211019235721_update_constraint_on_revoked_at.exs
+++ priv/repo/migrations/20211019235721_update_constraint_on_revoked_at.exs
@@ -7,9 +7,14 @@ defmodule Hexpm.RepoBase.Migrations.UpdateConstraintOnRevokedAt do
     execute(
       "CREATE UNIQUE INDEX keys_user_id_name_revoked_at_key ON keys (user_id, name) WHERE (revoked_at IS NULL)"
     )
+
+    execute(
+      "CREATE UNIQUE INDEX keys_organization_id_name_revoked_at_key ON keys (organization_id, name) WHERE (revoked_at IS NULL)"
+    )
   end
 
   def down do
+    execute("DROP INDEX keys_organization_id_name_revoked_at_key")
     execute("DROP INDEX keys_user_id_name_revoked_at_key")
 
     execute(
