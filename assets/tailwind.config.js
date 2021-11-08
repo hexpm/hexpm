2d7d30b7ddf6fbbae9787bf089499486f70f0f9b
TraceyOnim <traceyonim22@gmail.com>
2021-11-08 18:08:03 +0300

    Installing tailwind

---
+++ assets/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [
+    "../**/*.html.eex",
+    "../**/*.html.leex",
+    "../**/views/**/*.ex",
+    "../**/live/**/*.ex",
+    "./js/**/*.js"
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
--- config/dev.exs
+++ config/dev.exs
@@ -18,7 +18,15 @@ config :hexpm, HexpmWeb.Endpoint,
   check_origin: false,
   pubsub_server: Hexpm.PubSub,
   watchers: [
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    npx: [
+      "tailwindcss",
+      "--input=css/app.css",
+      "--output=../priv/static/assets/app.css",
+      "--postcss",
+      "--watch",
+      cd: Path.expand("../assets", __DIR__)
+    ]
   ]
 
 config :hexpm, HexpmWeb.Endpoint,
