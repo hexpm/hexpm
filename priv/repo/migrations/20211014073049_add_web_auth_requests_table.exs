e25a8813106f6938984e99542cab270d6c1d1aee
Benjamin Philip <benjamin.philip495@gmail.com>
2021-11-23 16:37:35 +0530

    Allow null for `:user_id` and `:audit` fields

--- priv/repo/migrations/20211014073049_add_web_auth_requests_table.exs
+++ priv/repo/migrations/20211014073049_add_web_auth_requests_table.exs
@@ -7,8 +7,8 @@ defmodule Hexpm.RepoBase.Migrations.AddWebAuthRequestsTable do
       add(:user_code, :string, null: false)
       add(:scope, :string, null: false)
       add(:verified, :boolean, null: false)
-      add(:user_id, :integer, null: false)
-      add(:audit, :string, null: false)
+      add(:user_id, :integer)
+      add(:audit, :string)
     end
   end
 end
