47fb521f3be355e38a9ae9480ccd01a36e0cebde
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2021-11-28 20:32:06 +0100

    Reserve resolver package names

--- lib/hexpm/repository/package.ex
+++ lib/hexpm/repository/package.ex
@@ -37,13 +37,7 @@ defmodule Hexpm.Repository.Package do
     nul con prn aux com1 com2 com3 com4 com5 com6 com7 com8 com9 lpt1 lpt2
     lpt3 lpt4 lpt5 lpt6 lpt7 lpt8 lpt9
   )
-
-  # Backwards compatible for tests, fixed in Hex at 2017-07-29
-  if Mix.env() == :hex do
-    @generic_names []
-  else
-    @generic_names ~w(package organization www)
-  end
+  @generic_names ~w(package organization www myapp lock locked)
 
   @reserved_names Enum.concat([
                     @elixir_names,
