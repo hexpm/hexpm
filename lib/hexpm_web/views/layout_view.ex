1c20be24254502013aac7fe159154882f6e4c263
Tracey Onim <43263401+TraceyOnim@users.noreply.github.com>
2021-12-10 16:30:12 +0300

    Style notifications (#1090)

--- lib/hexpm_web/views/layout_view.ex
+++ lib/hexpm_web/views/layout_view.ex
@@ -1,5 +1,6 @@
 defmodule HexpmWeb.LayoutView do
   use HexpmWeb, :view
+  use Phoenix.HTML
 
   def show_search?(assigns) do
     Map.get(assigns, :hide_search) != true
@@ -48,5 +49,29 @@ defmodule HexpmWeb.LayoutView do
       tag(:meta, property: "og:description", content: description(assigns)),
       tag(:meta, property: "og:site_name", content: "Hex")
     ]
+  end
+
+  def toaster_notification(flash, alert_type) do
+    ~E"""
+      <div class="relative top-2">
+        <div class="flex flex-row items-center justify-center h-11 max-w-max px-8  mx-auto overflow-hidden bg-white rounded-lg
+      shadow bg-white">
+          <div class="flex items-center space-x-4">
+            <div class="inline-flex rounded-full h-3.5 w-3.5 flex <%= notification_color(alert_type) %>"></div>
+            <p class="inline-flex font-medium text-base text-gray-700">
+              <%= raw flash %>
+            </p>
+          </div>
+        </div>
+      </div>
+    """
+  end
+
+  defp notification_color(alert_type) do
+    cond do
+      alert_type in [:raw_error, :error, :raw_warning, :warning] -> "bg-red-600"
+      alert_type in [:raw_info, :info, :raw_success, :success] -> "bg-green-600"
+      true -> ""
+    end
   end
 end
