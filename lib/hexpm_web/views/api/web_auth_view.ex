a4f9100fcf497d7ade0fa1aadb843c532d93ca44
Benjamin Philip <benjamin.philip495@gmail.com>
2021-12-20 16:53:53 +0530

    Support erlang responses

--- lib/hexpm_web/views/api/web_auth_view.ex
+++ lib/hexpm_web/views/api/web_auth_view.ex
@@ -1,11 +1,11 @@
 defmodule HexpmWeb.API.WebAuthView do
   use HexpmWeb, :view
 
-  def render("code.json", reponse) do
+  def render("code." <> _, reponse) do
     Map.take(reponse, [:device_code, :user_code])
   end
 
-  def render("access.json", %{write_key: write_key, read_key: read_key}) do
+  def render("access." <> _, %{write_key: write_key, read_key: read_key}) do
     %{
       write_key: write_key.user_secret,
       read_key: read_key.user_secret
