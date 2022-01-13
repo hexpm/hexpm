1206668db33b4ae7fbf825acd592c59454cd3d0e
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2022-01-13 13:54:30 +0100

    Fix policy pages

--- lib/hexpm_web/controllers/policy_controller.ex
+++ lib/hexpm_web/controllers/policy_controller.ex
@@ -2,47 +2,26 @@ defmodule HexpmWeb.PolicyController do
   use HexpmWeb, :controller
 
   def coc(conn, _params) do
-    render(
-      conn,
-      "coc.html",
-      title: "Code of Conduct",
-      container: "policy grid place-content-center space-y-8"
-    )
+    render_layout(conn, "coc.html", "Code of Conduct")
   end
 
   def copyright(conn, _params) do
-    render(
-      conn,
-      "copyright.html",
-      title: "Copyright Policy",
-      container: "policy grid place-content-center space-y-8"
-    )
+    render_layout(conn, "copyright.html", "Copyright Policy")
   end
 
   def privacy(conn, _params) do
-    render(
-      conn,
-      "privacy.html",
-      title: "Privacy Policy",
-      container: "policy grid place-content-center space-y-8"
-    )
+    render_layout(conn, "privacy.html", "Privacy Policy")
   end
 
   def tos(conn, _params) do
-    render(
-      conn,
-      "tos.html",
-      title: "Terms of Service",
-      container: "policy grid place-content-center space-y-8"
-    )
+    render_layout(conn, "tos.html", "Terms of Service")
   end
 
   def dispute(conn, _params) do
-    render(
-      conn,
-      "dispute.html",
-      title: "Naming Dispute Policy",
-      container: "policy grid place-content-center space-y-8"
-    )
+    render_layout(conn, "dispute.html", "Naming Dispute Policy")
+  end
+
+  defp render_layout(conn, view, title) do
+    render(conn, "layout.html", view: view, title: title, container: nil)
   end
 end
