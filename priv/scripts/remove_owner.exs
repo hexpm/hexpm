bbe32d600413e5a4be4615b87c9dc703e539d8db
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2022-02-17 18:00:22 +0100

    Fix remove_owner script

--- priv/scripts/remove_owner.exs
+++ priv/scripts/remove_owner.exs
@@ -1,5 +1,8 @@
 destructure [package, username_or_email], System.argv()
 
+alias Hexpm.Repository.{Owners, Packages}
+alias Hexpm.Accounts.Users
+
 package = Packages.get("hexpm", package)
 user = Users.get(username_or_email, [:emails])
 
