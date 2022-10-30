1200943886b08da3af4bc95af90c95715da7b141
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2022-10-30 02:54:45 +0200

    Add blog post for Hex 2.0

--- lib/hexpm_web/templates/blog/posts.ex
+++ lib/hexpm_web/templates/blog/posts.ex
@@ -5,7 +5,7 @@ defmodule HexpmWeb.Blog.Posts do
 
   all_templates =
     Phoenix.Template.find_all(@phoenix_root)
-    |> Enum.map(&Phoenix.Template.template_path_to_name(&1, @phoenix_root))
+    |> Enum.map(&Phoenix.View.template_path_to_name(&1, @phoenix_root))
     |> Enum.flat_map(fn
       <<n1, n2, n3, "-", slug::binary>> = template
       when n1 in ?0..?9 and n2 in ?0..?9 and n3 in ?0..?9 ->
