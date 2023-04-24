b3266aa00af3fa13ccc3b5dbe310d143ed844c2c
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2023-04-24 23:06:00 +0200

    Remove single version

--- priv/scripts/remove_package.exs
+++ priv/scripts/remove_package.exs
@@ -1,11 +1,11 @@
-destructure [name, repo], Enum.reverse(System.argv())
+destructure [repo, name, version], System.argv()
 
-repository =
-  if repo do
-    Hexpm.Repo.get_by!(Hexpm.Repository.Repository, name: name)
-  else
-    Hexpm.Repo.get!(Hexpm.Repository.Repository, 1)
-  end
+repository = Hexpm.Repo.get_by!(Hexpm.Repository.Repository, name: repo)
+
+unless repository do
+  IO.puts("No repository: #{repo}")
+  System.halt(1)
+end
 
 package =
   Hexpm.Repository.Package
@@ -17,21 +17,10 @@ unless package do
   System.halt(1)
 end
 
-releases =
-  Hexpm.Repository.Release.all(package)
-  |> Hexpm.Repo.all()
-  |> Hexpm.Repo.preload(package: :repository)
-
-package_owners =
-  Ecto.assoc(package, :package_owners)
-  |> Hexpm.Repo.all()
-
 owners =
   Ecto.assoc(package, :owners)
   |> Hexpm.Repo.all()
   |> Hexpm.Repo.preload(:emails)
-
-IO.puts(name)
 
 IO.puts("")
 IO.puts("Owners:")
@@ -40,20 +29,49 @@ Enum.each(owners, fn owner ->
   IO.puts("#{owner.username} #{Hexpm.Accounts.User.email(owner, :primary)}")
 end)
 
-IO.puts("")
-IO.puts("Releases:")
-Enum.each(releases, &IO.puts(&1.version))
+if version do
+  release =
+    Hexpm.Repository.Releases.get(package, version)
+    |> Hexpm.Repo.preload(package: :repository)
 
-answer = IO.gets("Remove? [Yn] ")
+    answer = IO.gets("Remove? [Yn] ")
 
-if answer =~ ~r/^(Y(es)?)?$/i do
-  Enum.each(package_owners, &Hexpm.Repo.delete!/1)
-  Enum.each(releases, &(Hexpm.Repository.Release.delete(&1, force: true) |> Hexpm.Repo.delete!()))
-  Hexpm.Repo.delete!(package)
-  Enum.each(releases, &Hexpm.Repository.Assets.revert_release/1)
-  Hexpm.Repository.RegistryBuilder.package_delete(package)
-  Hexpm.Repository.RegistryBuilder.repository(package.repository)
-  IO.puts("Removed")
+    if answer =~ ~r/^(Y(es)?)?$/i do
+      Hexpm.Repository.Release.delete(release, force: true)
+      |> Hexpm.Repo.delete!()
+
+      Hexpm.Repository.Assets.revert_release(release)
+      Hexpm.Repository.RegistryBuilder.package(package)
+      Hexpm.Repository.RegistryBuilder.repository(package.repository)
+      IO.puts("Removed")
+    else
+      IO.puts("Not removed")
+    end
 else
-  IO.puts("Not removed")
+  package_owners =
+    Ecto.assoc(package, :package_owners)
+    |> Hexpm.Repo.all()
+
+  releases =
+    Hexpm.Repository.Release.all(package)
+    |> Hexpm.Repo.all()
+    |> Hexpm.Repo.preload(package: :repository)
+
+  IO.puts("")
+  IO.puts("Releases:")
+  Enum.each(releases, &IO.puts(&1.version))
+
+  answer = IO.gets("Remove? [Yn] ")
+
+  if answer =~ ~r/^(Y(es)?)?$/i do
+    Enum.each(package_owners, &Hexpm.Repo.delete!/1)
+    Enum.each(releases, &(Hexpm.Repository.Release.delete(&1, force: true) |> Hexpm.Repo.delete!()))
+    Hexpm.Repo.delete!(package)
+    Enum.each(releases, &Hexpm.Repository.Assets.revert_release/1)
+    Hexpm.Repository.RegistryBuilder.package_delete(package)
+    Hexpm.Repository.RegistryBuilder.repository(package.repository)
+    IO.puts("Removed")
+  else
+    IO.puts("Not removed")
+  end
 end
