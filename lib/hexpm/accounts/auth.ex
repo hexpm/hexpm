e4ce9817f35074d3eca10656added28dafa37885
Holden Oullette <6202965+houllette@users.noreply.github.com>
2023-05-10 14:59:20 -0700

    switched away from anon func to use String.replace_prefix

--- lib/hexpm/accounts/auth.ex
+++ lib/hexpm/accounts/auth.ex
@@ -81,7 +81,7 @@ defmodule Hexpm.Accounts.Auth do
   def gen_key() do
     :crypto.strong_rand_bytes(16)
     |> Base.encode16(case: :lower)
-    |> (fn s -> "hexpm_#{s}" end).()
+    |> String.replace_prefix("", "hexpm_")
   end
 
   defp find_email(nil, _email) do
