4419618ce301a8b88f753617ef5cf9d3bef7d715
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2024-04-16 19:38:01 +0200

    Clean up Packages.diff

--- lib/hexpm/repository/packages.ex
+++ lib/hexpm/repository/packages.ex
@@ -11,14 +11,12 @@ defmodule Hexpm.Repository.Packages do
 
   def diff(packages, nil), do: packages
 
-  def diff(packages, remove) when is_list(remove) do
-    names = Enum.map(remove, & &1.name)
+  def diff(packages, remove) do
+    names = Enum.map(List.wrap(remove), & &1.name)
 
     packages
     |> Enum.reject(&(&1.name in names))
   end
-
-  def diff(packages, remove), do: diff(packages, [remove])
 
   def get(repository, name) when is_binary(repository) do
     repository = Repositories.get(repository)
