d7fe207a227b5d0cf2eb7c93321d2880c76de345
Todd Resudek <toddresudek@gmail.com>
2024-06-06 09:17:33 -0600

    IP

--- lib/hexpm_web/views/icons.ex
+++ lib/hexpm_web/views/icons.ex
@@ -1,135 +1,69 @@
 defmodule HexpmWeb.ViewIcons do
   use Phoenix.HTML
-  import SweetXml
 
   @icons_dir Path.join(__DIR__, "../../../assets/vendor/icons")
-  @octicons_svg Path.join(@icons_dir, "octicons.svg")
-  @glyphicons_svg Path.join(@icons_dir, "glyphicons-halflings-regular.svg")
-  @glyphicons_less Path.join(@icons_dir, "glyphicons.less")
+  @heroicons_svg Path.join(@icons_dir, "heroicons.svg")
 
-  @external_resource @octicons_svg
-  @external_resource @glyphicons_svg
-  @external_resource @glyphicons_less
+  @external_resource @heroicons_svg
 
-  :ok = Application.ensure_loaded(:xmerl)
-  {:ok, xmerl_version} = :application.get_key(:xmerl, :vsn)
+  # :ok = Application.ensure_loaded(:xmerl)
+  # {:ok, xmerl_version} = :application.get_key(:xmerl, :vsn)
 
-  xmerl_version =
-    xmerl_version
-    |> List.to_string()
-    |> String.split(".")
-    |> Enum.concat(["0"])
-    |> Enum.take(3)
-    |> Enum.join(".")
+  # xmerl_version =
+  #   xmerl_version
+  #   |> List.to_string
+  #   |> String.split(".")
+  #   |> Enum.concat(["0"])
+  #   |> Enum.take(3)
+  #   |> Enum.join(".")
 
-  broken_xmerl? = Version.compare(xmerl_version, "1.3.20") == :lt
+  # broken_xmerl? = Version.compare(xmerl_version, "1.3.20") == :lt
 
-  doc = File.read!(@octicons_svg)
+  doc = File.read!(@heroicons_svg)
 
-  octicons =
-    SweetXml.xpath(
-      doc,
-      ~x"//glyph"l,
-      name: ~x"./@glyph-name"s,
-      d: ~x"./@d"s,
-      x: ~x"./@horiz-adv-x"s
-    )
+  defp heroicon(title) when is_atom(title), do: heroicon(Atom.to_string(title))
 
-  defp octicon(name) when is_atom(name), do: octicon(Atom.to_string(name))
-
-  Enum.each(octicons, fn %{name: name, d: d, x: x} ->
-    defp octicon(unquote(name)), do: {unquote(d), unquote(x)}
-  end)
-
-  doc = File.read!(@glyphicons_svg)
-
-  glyphicons =
-    SweetXml.xpath(
-      doc,
-      ~x"//glyph[@unicode][@d]"l,
-      unicode: if(broken_xmerl?, do: ~x"./@unicode", else: ~x"./@unicode"s),
-      d: ~x"./@d"s,
-      x: ~x"./@horiz-adv-x"s
-    )
-
-  lines =
-    File.read!(@glyphicons_less)
-    |> String.split("\n", trim: true)
-
-  @glyphicon_less_regex ~r'\.glyphicon-([-\w]+)\s*\{ &:before \{ content: "\\([0-9a-f]{4})"; \} \}'
-  glyphicon_names =
-    Enum.reduce(lines, %{}, fn line, map ->
-      case Regex.run(@glyphicon_less_regex, line) do
-        [_, name, content] ->
-          Map.put(map, content, name)
-
-        nil ->
-          map
-      end
-    end)
-
-  defp glyphicon(name) when is_atom(name), do: glyphicon(Atom.to_string(name))
-
-  Enum.each(glyphicons, fn %{unicode: unicode, d: d, x: x} ->
-    unicode = if broken_xmerl?, do: IO.iodata_to_binary(Enum.reverse(unicode)), else: unicode
-
-    name =
-      case unicode do
-        <<char::utf8>> ->
-          codepoint =
-            char
-            |> Integer.to_string(16)
-            |> String.pad_leading(4, "0")
-            |> String.downcase()
-
-          Map.get(glyphicon_names, codepoint)
-      end
-
-    if name do
-      defp glyphicon(unquote(name)), do: {unquote(d), unquote(x)}
+  doc
+  |> String.split("\n")
+  |> Enum.each(fn line ->
+    case Regex.named_captures(~r/\<g title=\"(?<title>[\w\d\-]+)\"/, line) do
+      %{"title" => title} -> defp heroicon(unquote(title)), do: unquote(line)
+      _ -> :noop
     end
   end)
 
-  def icon(type, name, opts \\ [])
+  # Example return
+ #   %{
+#   title: ~c"tag",
+#   d: [~c"M9.56802 3H5.25C4.00736 3 3 4.00736 3 5.25V9.56802C3 10.1648 3.23705 10.7371 3.65901 11.159L13.2401 20.7401C13.9388 21.4388 15.0199 21.6117 15.8465 21.0705C17.9271 19.7084 19.7084 17.9271 21.0705 15.8465C21.6117 15.0199 21.4388 13.9388 20.7401 13.2401L11.159 3.65901C10.7371 3.23705 10.1648 3 9.56802 3Z",
+#    ~c"M6 6H6.0075V6.0075H6V6Z"],
+#   stroke: [~c"#0F172A", ~c"#0F172A"]
+# }
+# This will take the lists pulled from the above operation and reduce them into a single list of tuples
+# We should build that zipped list and then reduce to build the final tag
+# attrs = Enum.zip_reduce([tag[:d], tag[:stroke], tag[:stroke_width], tag[:stroke_linecap], tag[:stroke_linejoin]], [], fn elem, acc -> [List.to_tuple(elem) | acc] end)
+# Maybe this is much easier done by splitting the document by \n, and then using a regex to capture the title of the g tag ang build a keyword list where the key is that title and the value is the entire line
+# lines = String.split(doc, "\n")
+# Regex.named_captures(~r/\<g title=\"(?<title>[\w\d\-]+)\"/, sample)
+# %{"title" => "eye-of-3x3"}
 
-  def icon(:octicon, name, opts) do
-    class = "octicon octicon-#{name}"
-    {d, x} = octicon(name)
-    title = if title = opts[:title], do: content_tag(:title, title), else: ""
+
+  def icon(type, title, opts \\ [])
+
+  def icon(:heroicon, title, opts) do
+    class = "heroicon heroicon-#{title}"
+    g_tag = heroicon(title)
 
     opts =
       opts
       |> Keyword.put_new(:"aria-hidden", "true")
       |> Keyword.put_new(:version, "1.1")
-      |> Keyword.put_new(:viewBox, "0 0 #{x} 1024")
+      |> Keyword.put_new(:viewBox, "0 0 1024 1024")
+      |> Keyword.put_new(:title, title)
       |> Keyword.update(:class, class, &"#{class} #{&1}")
-      |> Keyword.drop([:title])
 
-    content_tag :svg, opts do
-      content_tag :g, transform: "translate(0, 800) scale(1, -1)" do
-        [content_tag(:path, "", d: d), title]
-      end
-    end
+    content_tag :svg, opts, do: to_string(g_tag)
   end
 
-  def icon(:glyphicon, name, opts) do
-    class = "glyphicon glyphicon-#{name}"
-    {d, x} = glyphicon(name)
-    x = if x == "", do: "1200", else: x
-    title = if title = opts[:title], do: content_tag(:title, title), else: ""
-
-    opts =
-      opts
-      |> Keyword.put_new(:"aria-hidden", "true")
-      |> Keyword.put_new(:version, "1.1")
-      |> Keyword.put_new(:viewBox, "0 0 #{x} 1200")
-      |> Keyword.update(:class, class, &"#{class} #{&1}")
-      |> Keyword.drop([:title])
-
-    content_tag :svg, opts do
-      content_tag :g, transform: "translate(0, 1200) scale(1, -1)" do
-        [content_tag(:path, "", d: d), title]
-      end
-    end
-  end
+  def icon(_any, _name, _opts), do: nil
 end
