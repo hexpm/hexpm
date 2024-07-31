57656b506517e5ac6345bee920a0f80d8fb2cc79
Wojtek Mach <wojtek@wojtekmach.pl>
2024-07-31 09:06:42 +0200

    Update priv/scripts/remove_owner.exs

--- priv/scripts/remove_owner.exs
+++ priv/scripts/remove_owner.exs
@@ -6,4 +6,7 @@ alias Hexpm.Accounts.Users
 package = Packages.get("hexpm", package)
 user = Users.get(username_or_email, [:emails])
 
-IO.inspect(Owners.remove(package, user, audit: {Users.get("admin"), "CLI", "127.0.0.1"}))
+Owners.remove(package, user,
+  audit: %{user: Users.get("admin"), user_agent: "CLI", remote_ip: "127.0.0.1", key: nil}
+)
+|> IO.inspect()
