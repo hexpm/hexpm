7a75291e35d0027defd2c31370fbbd8182731f65
Egor Mikhnevich <mikhnevich.egor@gmail.com>
2024-11-30 21:02:37 +0100

    Cleanup the webpack config

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -44,11 +44,6 @@ module.exports = (env, options) => ({
         test: /\.(png|woff|woff2|eot|ttf|svg)$/,
         loader: "file-loader",
         options: {outputPath: "css", publicPath: "/css"}
-      },
-      {
-        test: /bootstrap\/dist\/js\/umd\//,
-        loader: 'imports-loader',
-        options: {jQuery: 'jquery'}
       }
     ]
   },
@@ -56,23 +51,7 @@ module.exports = (env, options) => ({
     new MiniCssExtractPlugin({ filename: 'css/app.css' }),
     new CopyWebpackPlugin({patterns: [{ from: 'static/', to: '.' }]}),
     new webpack.ProvidePlugin({
-      $: "jquery",
       jQuery: "jquery",
-      "window.jQuery": "jquery",
-      Tether: "tether",
-      "window.Tether": "tether",
-      Popper: ['popper.js', 'default'],
-      Alert: "exports-loader?Alert!bootstrap/js/dist/alert",
-      Button: "exports-loader?Button!bootstrap/js/dist/button",
-      Carousel: "exports-loader?Carousel!bootstrap/js/dist/carousel",
-      Collapse: "exports-loader?Collapse!bootstrap/js/dist/collapse",
-      Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
-      Modal: "exports-loader?Modal!bootstrap/js/dist/modal",
-      Popover: "exports-loader?Popover!bootstrap/js/dist/popover",
-      Scrollspy: "exports-loader?Scrollspy!bootstrap/js/dist/scrollspy",
-      Tab: "exports-loader?Tab!bootstrap/js/dist/tab",
-      Tooltip: "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
-      Util: "exports-loader?Util!bootstrap/js/dist/util",
     })
   ]
 });
