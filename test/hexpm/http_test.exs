c7e78aadef057591083a54d281313c29eb4ab29d
Jonatan Männchen <jonatan@maennchen.ch>
2026-05-09 21:50:38 +0200

    Replace bypass with lasso test dependency

--- test/hexpm/http_test.exs
+++ test/hexpm/http_test.exs
@@ -5,60 +5,60 @@ defmodule Hexpm.HTTPTest do
   alias Plug.Conn
 
   setup do
-    bypass = Bypass.open()
-    {:ok, bypass: bypass}
+    lasso = Lasso.open()
+    {:ok, lasso: lasso}
   end
 
-  test "get/2", %{bypass: bypass} do
-    Bypass.expect_once(bypass, "GET", "/get", fn conn ->
+  test "get/2", %{lasso: lasso} do
+    Lasso.expect_once(lasso, "GET", "/get", fn conn ->
       Conn.resp(conn, 200, "respbody")
     end)
 
-    assert {:ok, 200, _headers, "respbody"} = HTTP.get(bypass_url(bypass, "/get"), [])
+    assert {:ok, 200, _headers, "respbody"} = HTTP.get(lasso_url(lasso, "/get"), [])
   end
 
-  test "post/3", %{bypass: bypass} do
-    Bypass.expect_once(bypass, "POST", "/post", fn conn ->
+  test "post/3", %{lasso: lasso} do
+    Lasso.expect_once(lasso, "POST", "/post", fn conn ->
       {:ok, reqbody, conn} = Conn.read_body(conn)
       assert reqbody == "reqbody"
       Conn.resp(conn, 200, "respbody")
     end)
 
     assert {:ok, 200, _headers, "respbody"} =
-             HTTP.post(bypass_url(bypass, "/post"), [], "reqbody")
+             HTTP.post(lasso_url(lasso, "/post"), [], "reqbody")
   end
 
-  test "put/3", %{bypass: bypass} do
-    Bypass.expect_once(bypass, "PUT", "/put", fn conn ->
+  test "put/3", %{lasso: lasso} do
+    Lasso.expect_once(lasso, "PUT", "/put", fn conn ->
       {:ok, reqbody, conn} = Conn.read_body(conn)
       assert reqbody == "reqbody"
       Conn.resp(conn, 200, "respbody")
     end)
 
-    assert {:ok, 200, _headers, "respbody"} = HTTP.put(bypass_url(bypass, "/put"), [], "reqbody")
+    assert {:ok, 200, _headers, "respbody"} = HTTP.put(lasso_url(lasso, "/put"), [], "reqbody")
   end
 
-  test "patch/3", %{bypass: bypass} do
-    Bypass.expect_once(bypass, "PATCH", "/patch", fn conn ->
+  test "patch/3", %{lasso: lasso} do
+    Lasso.expect_once(lasso, "PATCH", "/patch", fn conn ->
       {:ok, reqbody, conn} = Conn.read_body(conn)
       assert reqbody == "reqbody"
       Conn.resp(conn, 200, "respbody")
     end)
 
     assert {:ok, 200, _headers, "respbody"} =
-             HTTP.patch(bypass_url(bypass, "/patch"), [], "reqbody")
+             HTTP.patch(lasso_url(lasso, "/patch"), [], "reqbody")
   end
 
-  test "delete/2", %{bypass: bypass} do
-    Bypass.expect_once(bypass, "DELETE", "/delete", fn conn ->
+  test "delete/2", %{lasso: lasso} do
+    Lasso.expect_once(lasso, "DELETE", "/delete", fn conn ->
       Conn.resp(conn, 200, "respbody")
     end)
 
-    assert {:ok, 200, _headers, "respbody"} = HTTP.delete(bypass_url(bypass, "/delete"), [])
+    assert {:ok, 200, _headers, "respbody"} = HTTP.delete(lasso_url(lasso, "/delete"), [])
   end
 
-  test "post/3 encode json", %{bypass: bypass} do
-    Bypass.expect_once(bypass, "POST", "/post", fn conn ->
+  test "post/3 encode json", %{lasso: lasso} do
+    Lasso.expect_once(lasso, "POST", "/post", fn conn ->
       {:ok, reqbody, conn} = Conn.read_body(conn)
       assert Jason.decode(reqbody) == {:ok, %{"key" => "value"}}
       Conn.resp(conn, 200, "respbody")
@@ -68,11 +68,11 @@ defmodule Hexpm.HTTPTest do
     params = %{"key" => "value"}
 
     assert {:ok, 200, _headers, "respbody"} =
-             HTTP.post(bypass_url(bypass, "/post"), headers, params)
+             HTTP.post(lasso_url(lasso, "/post"), headers, params)
   end
 
-  test "post/3 encode urlencoded", %{bypass: bypass} do
-    Bypass.expect_once(bypass, "POST", "/post", fn conn ->
+  test "post/3 encode urlencoded", %{lasso: lasso} do
+    Lasso.expect_once(lasso, "POST", "/post", fn conn ->
       {:ok, reqbody, conn} = Conn.read_body(conn)
       assert URI.decode_query(reqbody) == %{"key" => "value"}
       Conn.resp(conn, 200, "respbody")
@@ -82,20 +82,20 @@ defmodule Hexpm.HTTPTest do
     params = %{"key" => "value"}
 
     assert {:ok, 200, _headers, "respbody"} =
-             HTTP.post(bypass_url(bypass, "/post"), headers, params)
+             HTTP.post(lasso_url(lasso, "/post"), headers, params)
   end
 
-  test "get/2 decode json", %{bypass: bypass} do
-    Bypass.expect_once(bypass, "GET", "/get", fn conn ->
+  test "get/2 decode json", %{lasso: lasso} do
+    Lasso.expect_once(lasso, "GET", "/get", fn conn ->
       conn
       |> Conn.put_resp_header("content-type", "application/json")
       |> Conn.resp(200, Jason.encode!(%{"key" => "value"}))
     end)
 
-    assert {:ok, 200, _headers, %{"key" => "value"}} = HTTP.get(bypass_url(bypass, "/get"), [])
+    assert {:ok, 200, _headers, %{"key" => "value"}} = HTTP.get(lasso_url(lasso, "/get"), [])
   end
 
-  defp bypass_url(bypass, path) do
-    "http://localhost:#{bypass.port}#{path}"
+  defp lasso_url(lasso, path) do
+    "http://localhost:#{lasso.port}#{path}"
   end
 end
