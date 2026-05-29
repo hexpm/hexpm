0c6610e689159a624151d5d4ad827299465aaaa0
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2026-05-29 14:02:05 +0200

    Reserve package names for organizations

--- lib/hexpm/accounts/organization.ex
+++ lib/hexpm/accounts/organization.ex
@@ -24,7 +24,7 @@ defmodule Hexpm.Accounts.Organization do
   @name_regex ~r"^[a-z0-9_\-\.]+$"
   @roles ~w(admin write read)
 
-  @reserved_names ~w(www staging elixir erlang otp rebar rebar3 phoenix acme search)
+  @reserved_names Enum.uniq(Hexpm.Repository.Package.reserved_names() ++ ~w(phoenix acme))
 
   def changeset(struct, params) do
     cast(struct, params, ~w(name)a)
